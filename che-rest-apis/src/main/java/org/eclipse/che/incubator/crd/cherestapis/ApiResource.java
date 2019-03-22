2899ea9771558fc675b6af57bdcce66282f908ba
David Festal <dfestal@redhat.com>
2019-03-22 10:51:39 +0100

    Add the `PUT` workspace update endpoint

--- che-rest-apis/src/main/java/org/eclipse/che/incubator/crd/cherestapis/ApiResource.java
+++ che-rest-apis/src/main/java/org/eclipse/che/incubator/crd/cherestapis/ApiResource.java
@@ -2,9 +2,11 @@ package org.eclipse.che.incubator.crd.cherestapis;
 
 import javax.inject.Inject;
 import javax.ws.rs.BadRequestException;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -28,4 +30,15 @@ public class ApiResource {
             throws NotFoundException, ServerErrorException, ForbiddenException, BadRequestException {
         return apiService.getWorkspace(key);
     }
+    
+    @PUT
+    @Path("workspace/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public WorkspaceDto update(
+        @PathParam("id") String id,
+        WorkspaceDto update) {
+      return apiService.getWorkspace(id);
+    }
+  
 }

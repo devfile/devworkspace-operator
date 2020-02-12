19fb8a927620e5be78a9f10071bf9955cc4a8669
Angel Misevski <amisevsk@redhat.com>
2020-02-11 21:41:42 -0500

    Trim ingress hostname if longer than 63 chars.
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- pkg/controller/modelutils/k8s/ingress.go
+++ pkg/controller/modelutils/k8s/ingress.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/che-incubator/che-workspace-operator/pkg/controller/workspace/config"
 )
@@ -10,10 +11,14 @@ import (
 // IngressHostname generates a hostname based on service name and namespace
 func IngressHostname(serviceName, namespace string, port int64) string {
 	ingressName := IngressName(serviceName, port)
-	return fmt.Sprintf("%s-%s.%s", ingressName, namespace, config.ControllerCfg.GetIngressGlobalDomain())
+	hostname := fmt.Sprintf("%s-%s", ingressName, namespace)
+	if len(hostname) > 63 {
+		hostname = strings.TrimSuffix(hostname[:63], "-")
+	}
+	return fmt.Sprintf("%s.%s", hostname, config.ControllerCfg.GetIngressGlobalDomain())
 }
 
-// IngressName genereates a names for ingresses
+// IngressName generates a names for ingresses
 func IngressName(serviceName string, port int64) string {
 	portString := strconv.FormatInt(port, 10)
 	return serviceName + "-" + portString

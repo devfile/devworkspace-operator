b70dba4ea20b7c35d75fe3f7fe208d46a59a68d7
Angel Misevski <amisevsk@redhat.com>
2020-03-03 14:34:27 -0500

    Remove che-rest-apis from repo
    Remove the Java-based che-rest-apis subproject from this repo in favor
    of https://github.com/che-incubator/che-api-sidecar/
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- che-rest-apis/src/test/java/org/eclipse/che/incubator/crd/cherestapis/NativeApiResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.che.incubator.crd.cherestapis;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeApiResourceIT extends ApiResourceTest {
-
-    // Execute the same tests but in native mode.
-}

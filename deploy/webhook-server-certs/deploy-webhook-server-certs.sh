01c93384b9b082b7319ce18c85059f809d83cc1a
Sergii Leshchenko <sleshche@redhat.com>
2020-03-20 15:20:52 +0200

    Separate instructions for k8s and OS
    Signed-off-by: Sergii Leshchenko <sleshche@redhat.com>

--- deploy/webhook-server-certs/deploy-webhook-server-certs.sh
+++ deploy/webhook-server-certs/deploy-webhook-server-certs.sh
@@ -18,6 +18,8 @@
 
 set -e
 
+CLI=$1
+
 echo "Generating new TLS certificates using docker"
 PROJECT_FOLDER=$(dirname "$0")/../..
 docker build --no-cache -t generate-webhook-server-certs:latest ${PROJECT_FOLDER}/deploy/webhook-server-certs
@@ -30,12 +32,12 @@ docker run --name 'webhook-certs' generate-webhook-server-certs:latest exit 0
 docker cp webhook-certs:ca/. ${TARGET_FOLDER}/
 docker rm 'webhook-certs'
 
-kubectl delete secret -n che-workspace-controller webhook-server-tls --ignore-not-found=true
-kubectl -n che-workspace-controller create secret tls webhook-server-tls \
+$CLI delete secret -n che-workspace-controller webhook-server-tls --ignore-not-found=true
+$CLI -n che-workspace-controller create secret tls webhook-server-tls \
     --cert "$TARGET_FOLDER/webhook-server-tls.crt" \
     --key "$TARGET_FOLDER/webhook-server-tls.key"
 CA_BASE_64_CONTENT="$(openssl base64 -A <"${TARGET_FOLDER}/ca.crt")"
-kubectl patch -n che-workspace-controller secret webhook-server-tls -p="{\"data\":{\"ca.crt\": \"${CA_BASE_64_CONTENT}\"}}"
+$CLI patch -n che-workspace-controller secret webhook-server-tls -p="{\"data\":{\"ca.crt\": \"${CA_BASE_64_CONTENT}\"}}"
 echo "TLS certificates are stored in 'che-workspace-controller' namespace in 'webhook-server-tls' secret"
 
 rm -r ${TARGET_FOLDER}

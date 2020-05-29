700394e52fdc6c6afee1bec1f262445a73be2733
Sergii Leshchenko <sleshche@redhat.com>
2020-05-29 14:01:31 +0300

    Align configuration properties names (#87)
    Signed-off-by: Sergii Leshchenko <sleshche@redhat.com>

--- pkg/config/property.go
+++ pkg/config/property.go
@@ -13,32 +13,32 @@
 package config
 
 const (
-	serverImageName        = "cherestapis.image.name"
-	defaultServerImageName = "quay.io/che-incubator/che-workspace-crd-rest-apis:7.1.0"
-
-	sidecarPullPolicy        = "sidecar.pull.policy"
-	defaultSidecarPullPolicy = "Always"
-
 	pluginRegistryURL = "plugin.registry.url"
 
 	routingSuffix        = "cluster.routing_suffix"
 	defaultRoutingSuffix = ""
 
+	webhooksEnabled        = "che.webhooks.enabled"
+	defaultWebhooksEnabled = "true"
+
+	serverImageName        = "che.workspace.che-api-sidecar.image"
+	defaultServerImageName = "quay.io/che-incubator/che-workspace-crd-rest-apis:7.1.0"
+
+	sidecarPullPolicy        = "che.workspace.sidecar.image_pull_policy"
+	defaultSidecarPullPolicy = "Always"
+
 	// workspacePVCName config property handles the PVC name that should be created and used for all workspaces within one kubernetes namespace
-	workspacePVCName        = "pvc.name"
+	workspacePVCName        = "che.workspace.pvc.name"
 	defaultWorkspacePVCName = "claim-che-workspace"
 
-	workspacePVCStorageClassName = "pvc.storage_class.name"
+	workspacePVCStorageClassName = "che.workspace_pvc.storage_class.name"
 
 	pluginArtifactsBrokerImage        = "che.workspace.plugin_broker.artifacts.image"
 	defaultPluginArtifactsBrokerImage = "quay.io/eclipse/che-plugin-artifacts-broker:v3.1.0"
 
 	// routingClass defines the default routing class that should be used if user does not specify it explicitly
-	routingClass        = "che.default_routing_class"
+	routingClass        = "che.workspace.default_routing_class"
 	defaultRoutingClass = "basic"
-
-	webhooksEnabled        = "che.webhooks.enabled"
-	defaultWebhooksEnabled = "true"
 
 	workspaceIdleTimeout        = "che.workspace.idle_timeout"
 	defaultWorkspaceIdleTimeout = "15m"

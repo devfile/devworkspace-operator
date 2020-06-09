d7ca3f946cad1c524e807cd1c80aa945dbf68c8c
Sergii Leshchenko <sleshche@redhat.com>
2020-06-09 14:12:44 +0300

    Minor fixes

--- .cico/cico_openshift_e2e.sh
+++ .cico/cico_openshift_e2e.sh
@@ -14,8 +14,8 @@ export CI="openshift" # ENV used by PROW ci to get the oc client
 export ARTIFACTS_DIR="/tmp/artifacts"
 export CUSTOM_HOMEDIR=$ARTIFACTS_DIR
 
-# Overrideable information
-export DEFAULT_INSTALLER_ASSETS_DIR=${DEFAULT_INSTALLER_ASSETS_DIR:-$(pwd)}
+# Overridable information
+export DEFAULT_INSTALLER_ASSETS_DIR=${DEFAULT_INSTALLER_ASSETS_DIR:-$(pwd)"/.e2e"}
 export KUBEADMIN_USER=${KUBEADMIN_USER:-"kubeadmin"}
 export KUBEADMIN_PASSWORD_FILE=${KUBEADMIN_PASSWORD_FILE:-"${DEFAULT_INSTALLER_ASSETS_DIR}/auth/kubeadmin-password"}
 
@@ -29,11 +29,12 @@ if [ ! -f $KUBEADMIN_PASSWORD_FILE ]; then
 fi
 
 # Get kubeadmin password from file
-export KUBEADMIN_PASSWORD=$(cat KUBEADMIN_PASSWORD_FILE)
+export KUBEADMIN_PASSWORD=$(cat $KUBEADMIN_PASSWORD_FILE)
 
 set +x
 # Login as admin user
 oc login -u $KUBEADMIN_USER -p $KUBEADMIN_PASSWORD --insecure-skip-tls-verify
+set -x
 
 # Output of e2e binary
 export OUT_FILE=bin/workspace-controller-e2e
@@ -41,7 +42,7 @@ export OUT_FILE=bin/workspace-controller-e2e
 # Compile e2e binary tests
 CGO_ENABLED=0 go test -v -c -o ${OUT_FILE} ./test/e2e/cmd/workspaces_test.go
 
-# Update CRDS
+# Update CRDs
 make update_devworkspace_crds
 
 # Launch tests

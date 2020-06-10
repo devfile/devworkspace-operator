8538ee3008053aa18d6edfcab1dfe3d48a189db6
flacatus <flacatus@redhat.com>
2020-06-10 20:42:00 +0200

    Modify CI scripts
    Signed-off-by: flacatus <flacatus@redhat.com>

--- .cico/cico_openshift_e2e.sh
+++ .cico/cico_openshift_e2e.sh
@@ -10,40 +10,29 @@
 set -ex
 
 # Setup to find necessary data from cluster setup
-export CI="openshift" # ENV used by PROW ci to get the oc client
+
+# ENV used by PROW ci
+export CI="openshift" 
 export ARTIFACTS_DIR="/tmp/artifacts"
-export CUSTOM_HOMEDIR=$ARTIFACTS_DIR
 
-# Overridable information
-export DEFAULT_INSTALLER_ASSETS_DIR=${DEFAULT_INSTALLER_ASSETS_DIR:-$(pwd)"/.e2e"}
-export KUBEADMIN_USER=${KUBEADMIN_USER:-"kubeadmin"}
-export KUBEADMIN_PASSWORD_FILE=${KUBEADMIN_PASSWORD_FILE:-"${DEFAULT_INSTALLER_ASSETS_DIR}/auth/kubeadmin-password"}
+mkdir -p $GOPATH/bin
+export PATH="$PATH:$(pwd):$GOPATH/bin"
+RELEASE_VERSION=v0.18.1
 
-# Exported to current env
-export KUBECONFIG=${KUBECONFIG:-"${DEFAULT_INSTALLER_ASSETS_DIR}/auth/kubeconfig"}
+curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
+chmod +x operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu && cp operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu $GOPATH/bin/operator-sdk && rm operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
 
-# Check if file with kubeadmin password exist
-if [ ! -f $KUBEADMIN_PASSWORD_FILE ]; then
-    echo "Could not find kubeadmin password file"
-    exit 1
-fi
+export GIT_COMMITTER_NAME="CI BOT"
+export GIT_COMMITTER_EMAIL="ci_bot@notused.com"
 
-# Get kubeadmin password from file
-export KUBEADMIN_PASSWORD=$(cat $KUBEADMIN_PASSWORD_FILE)
-
-set +x
-# Login as admin user
-oc login -u $KUBEADMIN_USER -p $KUBEADMIN_PASSWORD --insecure-skip-tls-verify
-set -x
+make update_devworkspace_crds
+go mod tidy
 
 # Output of e2e binary
 export OUT_FILE=bin/workspace-controller-e2e
 
 # Compile e2e binary tests
 CGO_ENABLED=0 go test -v -c -o ${OUT_FILE} ./test/e2e/cmd/workspaces_test.go
-
-# Update CRDs
-make update_devworkspace_crds
 
 # Launch tests
 ./bin/workspace-controller-e2e

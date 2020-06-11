12767374f8b64813d74070fef42a436ebe24f1fa
flacatus <flacatus@redhat.com>
2020-06-11 10:04:21 +0200

    Improve CI scripts
    Signed-off-by: flacatus <flacatus@redhat.com>

--- .cico/cico_openshift_e2e.sh
+++ .cico/cico_openshift_e2e.sh
@@ -9,23 +9,31 @@
 #
 set -ex
 
-# Setup to find necessary data from cluster setup
-
 # ENV used by PROW ci
 export CI="openshift" 
 export ARTIFACTS_DIR="/tmp/artifacts"
 
-mkdir -p $GOPATH/bin
-export PATH="$PATH:$(pwd):$GOPATH/bin"
-RELEASE_VERSION=v0.18.1
-
-curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
-chmod +x operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu && cp operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu $GOPATH/bin/operator-sdk && rm operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
-
+# Pod created by openshift ci don't have user. Using this envs should avoid errors with git user.
 export GIT_COMMITTER_NAME="CI BOT"
 export GIT_COMMITTER_EMAIL="ci_bot@notused.com"
 
+# Check if operator-sdk is installed and if not install operator sdk in $GOPATH/bin dir
+if ! hash operator-sdk 2>/dev/null; then
+    mkdir -p $GOPATH/bin
+    export PATH="$PATH:$(pwd):$GOPATH/bin"
+    RELEASE_VERSION=v0.17.0
+
+    curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
+
+    chmod +x operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu && \
+        cp operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu $GOPATH/bin/operator-sdk && \
+        rm operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
+fi
+
+# Add kubernetes-api CRDS
 make update_devworkspace_crds
+
+# Install go modules
 go mod tidy
 go mod vendor
 

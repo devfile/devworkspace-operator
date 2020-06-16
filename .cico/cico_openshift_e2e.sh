c7c2ca5957b0a40b5077a644c8dfdd4b968dc34b
flacatus <flacatus@redhat.com>
2020-06-16 15:12:29 +0200

    Test triggered jobs
    Signed-off-by: flacatus <flacatus@redhat.com>

--- .cico/cico_openshift_e2e.sh
+++ .cico/cico_openshift_e2e.sh
@@ -16,7 +16,7 @@ export ARTIFACTS_DIR="/tmp/artifacts"
 # Pod created by openshift ci don't have user. Using this envs should avoid errors with git user.
 export GIT_COMMITTER_NAME="CI BOT"
 export GIT_COMMITTER_EMAIL="ci_bot@notused.com"
-
+####TEEEST
 # Check if operator-sdk is installed and if not install operator sdk in $GOPATH/bin dir
 if ! hash operator-sdk 2>/dev/null; then
     mkdir -p $GOPATH/bin

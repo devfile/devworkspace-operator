468a356002467b54814828d7b8e9a582abe8c609
Flavius Lacatusu <flacatus@redhat.com>
2020-07-15 23:09:15 +0200

    Update main.go

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -53,7 +53,7 @@ var (
 var log = logf.Log.WithName("cmd")
 
 func printVersion() {
-	log.Info(fmt.Sprintf("Operator Version: %s", version.Version))
+	log.Info(fmt.Sprintf("The Operator Version is.....................................: %s", version.Version))
 	log.Info(fmt.Sprintf("Go Version: %s", runtime.Version()))
 	log.Info(fmt.Sprintf("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH))
 	log.Info(fmt.Sprintf("Version of operator-sdk: %v", sdkVersion.Version))

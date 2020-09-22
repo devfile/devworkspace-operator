12b6c1057a1323e673893bfc59fc1d35f3dbac8c
Flavius Lacatusu <flacatus@redhat.com>
2020-09-22 15:15:00 +0200

    Fix ci script and add operator image environment created by openshift ci
    Signed-off-by: Flavius Lacatusu <flacatus@redhat.com>

--- .cico/cico_openshift_e2e.sh
+++ .cico/cico_openshift_e2e.sh
@@ -7,24 +7,36 @@
 #
 # SPDX-License-Identifier: EPL-2.0
 #
-set -ex
+
+#!/usr/bin/env bash
+# exit immediately when a command fails
+set -e
+# only exit with zero if all commands of the pipeline exit successfully
+set -o pipefail
+# error on unset variables
+set -u
+# print each command before executing it
+set -x
 
 trap 'Catch_Finish $?' EXIT SIGINT
 
 # Catch the finish of the job and write logs in artifacts.
 function Catch_Finish() {
     # grab devworkspace-controller namespace events after running e2e
-    oc get events -n devworkspace-controller | tee ${ARTIFACTS_DIR}/devworkspace-controller-events.log
+    getCheClusterLogs
 }
 
 # ENV used by PROW ci
 export CI="openshift"
 export ARTIFACTS_DIR="/tmp/artifacts"
+export NAMESPACE="devworkspace-controller"
 
 # Component is defined in Openshift CI job configuration. See: https://github.com/openshift/release/blob/master/ci-operator/config/devfile/devworkspace-operator/devfile-devworkspace-operator-master__v4.yaml#L8
 export CI_COMPONENT="devworkspace-operator"
-# OPENSHIFT_BUILD_NAMESPACE env var exposed by Openshift CI. More info about how images are builded in Openshift CI: https://github.com/openshift/ci-tools/blob/master/TEMPLATES.md#parameters-available-to-templates
-export IMG=default-route-openshift-image-registry.apps.build01.ci.devcluster.openshift.com/${OPENSHIFT_BUILD_NAMESPACE}/stable:${CI_COMPONENT}
+
+# DEVWORKSACE_OPERATOR env var exposed by Openshift CI in e2e test pod. More info about how images are builded in Openshift CI: https://github.com/openshift/ci-tools/blob/master/TEMPLATES.md#parameters-available-to-templates
+# Dependencies environment are defined here: https://github.com/openshift/release/blob/master/ci-operator/config/devfile/devworkspace-operator/devfile-devworkspace-operator-master__v5.yaml#L36-L38
+export IMG=${DEVWORKSACE_OPERATOR}
 
 # Pod created by openshift ci don't have user. Using this envs should avoid errors with git user.
 export GIT_COMMITTER_NAME="CI BOT"
@@ -43,9 +55,24 @@ if ! hash operator-sdk 2>/dev/null; then
         rm operator-sdk-${OPERATOR_SDK_VERSION}-x86_64-linux-gnu
 fi
 
+# Function to get all logs and events from devworkspace operator deployments
+function getCheClusterLogs() {
+    mkdir -p ${ARTIFACTS_DIR}/devworkspace-operator
+    cd ${ARTIFACTS_DIR}/devworkspace-operator
+    for POD in $(oc get pods -o name -n ${NAMESPACE}); do
+       for CONTAINER in $(oc get -n ${NAMESPACE} ${POD} -o jsonpath="{.spec.containers[*].name}"); do
+            echo ""
+            echo "<=========================Getting logs from $POD==================================>"
+            echo ""
+            oc logs ${POD} -c ${CONTAINER} -n ${NAMESPACE} | tee $(echo ${POD}-${CONTAINER}.log | sed 's|pod/||g')
+        done
+    done
+    echo "======== oc get events ========"
+    oc get events -n ${NAMESPACE}| tee get_events.log
+}
+
 # For some reason go on PROW force usage vendor folder
 # This workaround is here until we don't figure out cause
 go mod tidy
 go mod vendor
-
 make test_e2e

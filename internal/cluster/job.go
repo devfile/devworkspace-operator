e026fea48a80a8e256848b3aebe8bdf2031aab52
Josh Pinkney <joshpinkney@gmail.com>
2020-10-05 08:08:17 -0400

    Fixup
    Signed-off-by: Josh Pinkney <joshpinkney@gmail.com>

--- internal/cluster/job.go
+++ internal/cluster/job.go
@@ -15,10 +15,11 @@ package cluster
 import (
 	"context"
 	"fmt"
+	"time"
+
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
-	"time"
 
 	batchv1 "k8s.io/api/batch/v1"
 	"k8s.io/apimachinery/pkg/types"

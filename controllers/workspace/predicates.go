607c6a9e915050204d560733c120a19658e2fe42
Angel Misevski <amisevsk@redhat.com>
2020-12-21 14:23:07 -0500

    Trigger failed workspace reconciles whenever spec is changed
    This fixes two issues:
    - We were ignoring changes to the `started` state of the workspace (it
    could not be restarted to fix a failure)
    - It allows us to check any spec changes -- if e.g. a typo in a plugin
    ID is fixed it allows the workspace to start.
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- controllers/workspace/predicates.go
+++ controllers/workspace/predicates.go
@@ -14,10 +14,14 @@ package controllers
 
 import (
 	"github.com/devfile/api/pkg/apis/workspaces/v1alpha2"
+	"k8s.io/apimachinery/pkg/api/equality"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 )
 
+// predicates filters incoming events to avoid unnecessary reconciles to failed workspaces.
+// If a workspace failed and its spec is changed, we trigger reconciles to allow for fixing
+// issues in the workspace spec.
 var predicates = predicate.Funcs{
 	CreateFunc: func(_ event.CreateEvent) bool { return true },
 	DeleteFunc: func(_ event.DeleteEvent) bool { return true },
@@ -34,8 +38,8 @@ var predicates = predicate.Funcs{
 			// Should never happen
 			return true
 		}
-		// Trigger a reconcile on failed workspaces if routingClass is updated.
-		return oldObj.Spec.RoutingClass != newObj.Spec.RoutingClass
+		// Trigger a reconcile on failed workspaces if spec is updated.
+		return !equality.Semantic.DeepEqual(oldObj.Spec, newObj.Spec)
 	},
 	GenericFunc: func(_ event.GenericEvent) bool { return true },
 }

22fa39e9d1edbef04c16321de05026f106bdf64b
Angel Misevski <amisevsk@redhat.com>
2021-01-11 17:54:51 -0500

    Improve plugins support in flatten library
    * Add functionality for detecting reference cycles in plugins (e.g.
    plugin A references plugin B which references plugin A), avoiding the
    potential to lock the operator
    * Add basic functionality for supporting compatibility between plugins
    and editors, based on annotations applied to DevWorkspaceTemplates
    representing plugins; some work would be required for similar
    functionality for plugins specified by e.g. ID.
    
    This is done by building a tree of plugin information as we flatten
    plugin imports; each node in the tree stores the information about the
    plugin, a node representing the component that imported it, and a list
    of nodes representing plugins it imports. We can walk this tree to check
    e.g. plugin labels for all plugins included in the workspace (to check
    compatiblity) or to look for import loops (by following parent
    references).
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

---
+++ pkg/library/flatten/helper.go
@@ -0,0 +1,67 @@
+//
+// Copyright (c) 2019-2020 Red Hat, Inc.
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+//
+// SPDX-License-Identifier: EPL-2.0
+//
+// Contributors:
+//   Red Hat, Inc. - initial API and implementation
+//
+
+package flatten
+
+import (
+	"fmt"
+	"reflect"
+
+	devworkspace "github.com/devfile/api/pkg/apis/workspaces/v1alpha2"
+)
+
+// resolutionContextTree is a recursive structure representing information about the devworkspace that is
+// lost when flattening
+type resolutionContextTree struct {
+	componentName   string
+	importReference devworkspace.ImportReference
+	pluginMetadata  map[string]string
+	plugins         []*resolutionContextTree
+	parentNode      *resolutionContextTree
+}
+
+func (t *resolutionContextTree) addPlugin(name string, plugin *devworkspace.PluginComponent) *resolutionContextTree {
+	newNode := &resolutionContextTree{
+		componentName:   name,
+		importReference: plugin.ImportReference,
+		parentNode:      t,
+	}
+	t.plugins = append(t.plugins, newNode)
+	return newNode
+}
+
+func (t *resolutionContextTree) hasCycle() error {
+	var seenRefs []devworkspace.ImportReference
+	currNode := t
+	for currNode.parentNode != nil {
+		for _, seenRef := range seenRefs {
+			if reflect.DeepEqual(seenRef, currNode.importReference) {
+				return fmt.Errorf("DevWorkspace has an cycle in references: %s", formatImportCycle(t))
+			}
+		}
+		seenRefs = append(seenRefs, currNode.importReference)
+		currNode = currNode.parentNode
+	}
+	return nil
+}
+
+func formatImportCycle(end *resolutionContextTree) string {
+	cycle := fmt.Sprintf("%s", end.componentName)
+	for end.parentNode != nil {
+		end = end.parentNode
+		if end.parentNode == nil {
+			end.componentName = "devworkspace"
+		}
+		cycle = fmt.Sprintf("%s -> %s", end.componentName, cycle)
+	}
+	return cycle
+}

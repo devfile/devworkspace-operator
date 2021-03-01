8cd528a38cd5f325bd4de1f8483b77568e8a0086
Angel Misevski <amisevsk@redhat.com>
2021-03-01 17:34:41 -0500

    Limit async workspaces to one and implement basic cleanup
    Limit the number of started async workspaces to one, to avoid some
    issues when a workspace is deleted while another workspace is using
    async storage.
    
    Implement a basic storage finalizing process for async workspaces, that
    scales the async server to 0 before running the common PVC cleanup job.
    This would fail when another workspace is currently running.
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- pkg/provision/storage/asyncStorage.go
+++ pkg/provision/storage/asyncStorage.go
@@ -20,7 +20,8 @@ import (
 	dw "github.com/devfile/api/v2/pkg/apis/workspaces/v1alpha2"
 	"github.com/devfile/devworkspace-operator/apis/controller/v1alpha1"
 	"github.com/devfile/devworkspace-operator/controllers/workspace/provision"
-	"github.com/devfile/devworkspace-operator/pkg/library/constants"
+	"github.com/devfile/devworkspace-operator/pkg/constants"
+	devfileConstants "github.com/devfile/devworkspace-operator/pkg/library/constants"
 	"github.com/devfile/devworkspace-operator/pkg/provision/storage/asyncstorage"
 
 	corev1 "k8s.io/api/core/v1"
@@ -40,9 +41,23 @@ func (*AsyncStorageProvisioner) NeedsStorage(workspace *dw.DevWorkspaceTemplateS
 }
 
 func (p *AsyncStorageProvisioner) ProvisionStorage(podAdditions *v1alpha1.PodAdditions, workspace *dw.DevWorkspace, clusterAPI provision.ClusterAPI) error {
+	numWorkspaces, _, err := p.getAsyncWorkspaceCount(clusterAPI)
+	if err != nil {
+		return err
+	}
+	// If there is more than one started workspace using async storage, then we fail starting additional ones
+	// Note we need to check phase so as to not accidentally fail an already-running workspace when a second one
+	// is created.
+	if numWorkspaces > 1 && workspace.Status.Phase != dw.WorkspaceStatusRunning {
+		return &ProvisioningError{
+			Message: fmt.Sprintf("Cannot provision storage for workspace %s", workspace.Name),
+			Err:     fmt.Errorf("at most one workspace using async storage can be running in a namespace"),
+		}
+	}
+
 	// Add ephemeral volumes
 	_, ephemeralVolumes, projectsVolume := getWorkspaceVolumes(workspace)
-	_, err := addEphemeralVolumesToPodAdditions(podAdditions, ephemeralVolumes)
+	_, err = addEphemeralVolumesToPodAdditions(podAdditions, ephemeralVolumes)
 	if err != nil {
 		return &ProvisioningError{Message: "Failed to add ephemeral volumes to workspace", Err: err}
 	}
@@ -125,7 +140,67 @@ func (p *AsyncStorageProvisioner) ProvisionStorage(podAdditions *v1alpha1.PodAdd
 	return nil
 }
 
-func (*AsyncStorageProvisioner) CleanupWorkspaceStorage(workspace *dw.DevWorkspace, clusterAPI provision.ClusterAPI) error {
+func (p *AsyncStorageProvisioner) CleanupWorkspaceStorage(workspace *dw.DevWorkspace, clusterAPI provision.ClusterAPI) error {
+	// TODO: This approach relies on there being a maximum of one workspace running per namespace.
+	asyncDeploy, err := asyncstorage.GetWorkspaceSyncDeploymentCluster(workspace.Namespace, clusterAPI)
+	if err != nil {
+		if k8sErrors.IsNotFound(err) {
+			return runCommonPVCCleanupJob(workspace, clusterAPI)
+		} else {
+			return err
+		}
+	}
+
+	// Check if another workspace is currently using the async server
+	numWorkspaces, totalWorkspaces, err := p.getAsyncWorkspaceCount(clusterAPI)
+	if err != nil {
+		return err
+	}
+	switch numWorkspaces {
+	case 0:
+		// no problem
+	case 1:
+		if workspace.Spec.Started {
+			// This is the only workspace using the async server, we can safely stop it
+			break
+		}
+		// Another async workspace is currently running; we can't safely clean up
+		return &ProvisioningError{
+			Message: "Cannot cleanup storage",
+			Err:     fmt.Errorf("another workspace is using the async server"),
+		}
+	default:
+		return &ProvisioningError{
+			Message: "Cannot cleanup storage",
+			Err:     fmt.Errorf("multiple workspaces are using using the async server"),
+		}
+	}
+
+	// Scale async deployment to zero to free up common PVC
+	currReplicas := asyncDeploy.Spec.Replicas
+	if currReplicas == nil || *currReplicas != 0 {
+		intzero := int32(0)
+		asyncDeploy.Spec.Replicas = &intzero
+		err := clusterAPI.Client.Update(clusterAPI.Ctx, asyncDeploy)
+		if err != nil && !k8sErrors.IsConflict(err) {
+			return err
+		}
+		return &NotReadyError{Message: "Scaling down async storage deployment to 0"}
+	}
+
+	// Clean up PVC using usual job
+	err = runCommonPVCCleanupJob(workspace, clusterAPI)
+	if err != nil {
+		return err
+	}
+
+	// Delete the async deployment if there are no workspaces except for the one being deleted
+	if totalWorkspaces <= 1 {
+		err := clusterAPI.Client.Delete(clusterAPI.Ctx, asyncDeploy)
+		if err != nil && !k8sErrors.IsNotFound(err) {
+			return err
+		}
+	}
 	return nil
 }
 
@@ -148,7 +223,7 @@ func (*AsyncStorageProvisioner) addVolumesForAsyncStorage(podAdditions *v1alpha1
 			volumes = append(volumes, vol...)
 		} else {
 			vol := corev1.Volume{
-				Name: constants.ProjectsVolumeName,
+				Name: devfileConstants.ProjectsVolumeName,
 				VolumeSource: corev1.VolumeSource{
 					EmptyDir: &corev1.EmptyDirVolumeSource{},
 				},
@@ -159,4 +234,25 @@ func (*AsyncStorageProvisioner) addVolumesForAsyncStorage(podAdditions *v1alpha1
 	}
 
 	return volumes, nil
+}
+
+// checkCanStartWorkspace returns whether the async storage provider can support starting a workspace.
+// Due to how cleanup for the async storage PVC is implemented, only one workspace that uses the async storage
+// type can be running at a time.
+func (*AsyncStorageProvisioner) getAsyncWorkspaceCount(api provision.ClusterAPI) (started, total int, err error) {
+	workspaces := &dw.DevWorkspaceList{}
+	err = api.Client.List(api.Ctx, workspaces)
+	if err != nil {
+		return 0, 0, err
+	}
+	for _, workspace := range workspaces.Items {
+		if workspace.Labels[constants.WorkspaceStorageTypeLabel] == constants.AsyncStorageClassType {
+			total++
+			if workspace.Spec.Started {
+				started++
+			}
+		}
+
+	}
+	return started, total, nil
 }

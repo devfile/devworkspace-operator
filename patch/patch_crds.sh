dd8ba46a37096a295aefcbf71d9522aff7df6d6a
Angel Misevski <amisevsk@redhat.com>
2021-03-18 14:24:08 -0400

    Remove Component CRD and related code
    Remove the components.controller.devfile.io Custom Resource Definition
    from the repo, as it is unused in adopting devfile 2.0 more directly
    (pkg/flatten).
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- patch/patch_crds.sh
+++ patch/patch_crds.sh
@@ -20,7 +20,6 @@
 
 set -e
 
-COMPONENTS_CRD_PATH="deploy/templates/crd/bases/controller.devfile.io_components.yaml"
 ROUTINGS_CRD_PATH="deploy/templates/crd/bases/controller.devfile.io_devworkspaceroutings.yaml"
 
 # CRD path from root to status field, in jq filter format
@@ -30,26 +29,8 @@ STATUS_PATH='.spec.versions[].schema.openAPIV3Schema.properties["status"]'
 # "containers" or "initContainers", as both have containerPorts that must be patched.
 PODADDITIONS_PATH='.properties["podAdditions"].properties[$CONTAINERS_FIELD].items.properties["ports"].items.required'
 
-# Full jq path to container ports required field in components CRD.
-COMPONENTS_SPEC_PATH="${STATUS_PATH}"'.properties["componentDescriptions"].items'"${PODADDITIONS_PATH}"
 # Full jq path to container ports required field in devworkspaceroutings CRD.
 ROUTINGS_SPEC_PATH="${STATUS_PATH}${PODADDITIONS_PATH}"
-
-# Update components CRD using yq; no-op if already patched.
-# Args:
-#  $1 - podAdditions field to update ("containers" or "initContainers")
-function update_components_crd() {
-  field="$1"
-  local yq_check_script="${COMPONENTS_SPEC_PATH}"' | index("protocol")'
-  local yq_patch_script="${COMPONENTS_SPEC_PATH}"' |= . + ["protocol"]'
-  already_patched=$(yq -r "$yq_check_script" "$COMPONENTS_CRD_PATH" --arg "CONTAINERS_FIELD" "$field")
-  if [[ "$already_patched" == "null" ]]; then
-    yq -Y -i "$yq_patch_script" "$COMPONENTS_CRD_PATH" --arg "CONTAINERS_FIELD" "$field"
-    echo "Updated CRD $COMPONENTS_CRD_PATH"
-  else
-    echo "Updating CRD $COMPONENTS_CRD_PATH not necessary"
-  fi
-}
 
 # Update devworkspaceroutings CRD using yq; no-op if already patched.
 # Args:
@@ -72,7 +53,5 @@ if ! command -v yq 2> /dev/null; then
   exit 1
 fi
 
-update_components_crd "containers"
-update_components_crd "initContainers"
 update_routings_crd "containers"
 update_routings_crd "initContainers"

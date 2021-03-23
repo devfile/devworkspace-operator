771026f307bd285c35ec6c1a4257cb077695a7e9
Angel Misevski <amisevsk@redhat.com>
2021-03-23 15:55:13 -0400

    Make resolving utility functions type agnostic
    Make the utility functions used for resolving plugins by ID, URI, or
    Kubernetes reference not depend on the PluginComponent type and instead
    take the relevant information directly. This is required to allow
    reusing these functions for resolving Devfile parents
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- controllers/workspace/devworkspace_controller.go
+++ controllers/workspace/devworkspace_controller.go
@@ -162,11 +162,11 @@ func (r *DevWorkspaceReconciler) Reconcile(req ctrl.Request) (reconcileResult ct
 	timing.SetTime(timingInfo, timing.ComponentsCreated)
 	// TODO#185 : Temporarily do devfile flattening in main reconcile loop; this should be moved to a subcontroller.
 	flattenHelpers := flatten.ResolverTools{
-		InstanceNamespace: workspace.Namespace,
-		Context:           ctx,
-		K8sClient:         r.Client,
-		InternalRegistry:  &registry.InternalRegistryImpl{},
-		HttpClient:        http.DefaultClient,
+		DefaultNamespace: workspace.Namespace,
+		Context:          ctx,
+		K8sClient:        r.Client,
+		InternalRegistry: &registry.InternalRegistryImpl{},
+		HttpClient:       http.DefaultClient,
 	}
 	flattenedWorkspace, err := flatten.ResolveDevWorkspace(workspace.Spec.Template, flattenHelpers)
 	if err != nil {

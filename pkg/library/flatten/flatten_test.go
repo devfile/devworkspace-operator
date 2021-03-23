3a17bd4c54feb3f7f8ebc00502ea173ed3e4eb72
Angel Misevski <amisevsk@redhat.com>
2021-03-23 16:33:34 -0400

    Add test cases to cover cases where incomplete tooling is provided
    Adds test cases to cover:
    - Using resolver when HttpClient or K8sClient are nil
    - Resolving Kubernetes resources when no namespace can be determined
    - Resolving resources by ID when no registryURL can be determined
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- pkg/library/flatten/flatten_test.go
+++ pkg/library/flatten/flatten_test.go
@@ -179,3 +179,41 @@ func TestResolveDevWorkspaceParents(t *testing.T) {
 		})
 	}
 }
+
+func TestResolveDevWorkspaceMissingDefaults(t *testing.T) {
+	tests := []testutil.TestCase{
+		testutil.LoadTestCaseOrPanic(t, "testdata/general/fail-nicely-when-no-registry-url.yaml"),
+		testutil.LoadTestCaseOrPanic(t, "testdata/general/fail-nicely-when-no-namespace.yaml"),
+	}
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			// sanity check: input defines components
+			assert.True(t, len(tt.Input.Workspace.Components) > 0, "Test case defines workspace with no components")
+			testHttpGetter := &testutil.FakeHTTPGetter{
+				DevfileResources:      tt.Input.DevfileResources,
+				DevWorkspaceResources: tt.Input.DevWorkspaceResources,
+				Errors:                tt.Input.Errors,
+			}
+			testK8sClient := &testutil.FakeK8sClient{
+				DevWorkspaceResources: tt.Input.DevWorkspaceResources,
+				Errors:                tt.Input.Errors,
+			}
+			testResolverTools := ResolverTools{
+				Context:    context.Background(),
+				K8sClient:  testK8sClient,
+				HttpClient: testHttpGetter,
+			}
+			outputWorkspace, err := ResolveDevWorkspace(tt.Input.Workspace, testResolverTools)
+			if tt.Output.ErrRegexp != nil && assert.Error(t, err) {
+				assert.Regexp(t, *tt.Output.ErrRegexp, err.Error(), "Error message should match")
+			} else {
+				if !assert.NoError(t, err, "Should not return error") {
+					return
+				}
+				assert.Truef(t, cmp.Equal(tt.Output.Workspace, outputWorkspace, testutil.WorkspaceTemplateDiffOpts),
+					"Workspace should match expected output:\n%s",
+					cmp.Diff(tt.Output.Workspace, outputWorkspace, testutil.WorkspaceTemplateDiffOpts))
+			}
+		})
+	}
+}

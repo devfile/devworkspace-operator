62e828410a4f322f4f1a79779ce697fd3d9caa66
Angel Misevski <amisevsk@redhat.com>
2021-03-23 23:15:31 -0400

    Add tests to cover attributes on merged devfiles
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- pkg/library/flatten/flatten_test.go
+++ pkg/library/flatten/flatten_test.go
@@ -217,3 +217,39 @@ func TestResolveDevWorkspaceMissingDefaults(t *testing.T) {
 		})
 	}
 }
+
+func TestResolveDevWorkspaceAnnotations(t *testing.T) {
+	tests := testutil.LoadAllTestsOrPanic(t, "testdata/annotate")
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			// sanity check: input defines components
+			assert.True(t, len(tt.Input.Workspace.Components) > 0, "Test case defines workspace with no components")
+			testHttpGetter := &testutil.FakeHTTPGetter{
+				DevfileResources:      tt.Input.DevfileResources,
+				DevWorkspaceResources: tt.Input.DevWorkspaceResources,
+				Errors:                tt.Input.Errors,
+			}
+			testK8sClient := &testutil.FakeK8sClient{
+				DevWorkspaceResources: tt.Input.DevWorkspaceResources,
+				Errors:                tt.Input.Errors,
+			}
+			testResolverTools := ResolverTools{
+				Context:          context.Background(),
+				K8sClient:        testK8sClient,
+				HttpClient:       testHttpGetter,
+				DefaultNamespace: "default-namespace",
+			}
+			outputWorkspace, err := ResolveDevWorkspace(tt.Input.Workspace, testResolverTools)
+			if tt.Output.ErrRegexp != nil && assert.Error(t, err) {
+				assert.Regexp(t, *tt.Output.ErrRegexp, err.Error(), "Error message should match")
+			} else {
+				if !assert.NoError(t, err, "Should not return error") {
+					return
+				}
+				assert.Truef(t, cmp.Equal(tt.Output.Workspace, outputWorkspace, testutil.WorkspaceTemplateDiffOpts),
+					"Workspace should match expected output:\n%s",
+					cmp.Diff(tt.Output.Workspace, outputWorkspace, testutil.WorkspaceTemplateDiffOpts))
+			}
+		})
+	}
+}

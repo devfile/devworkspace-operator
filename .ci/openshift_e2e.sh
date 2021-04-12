3049e3a5a3ef90890701a85f3d32f3df05ee0bbd
Skorik Sergey <sskoryk@redhat.com>
2021-04-12 15:16:56 +0300

    start happy-path test checking from oci-devworkspace-happy-path.sh

--- .ci/openshift_e2e.sh
+++ .ci/openshift_e2e.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2012-2021 Red Hat, Inc.
+# Copyright (c) 2012-2020 Red Hat, Inc.
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -28,12 +28,14 @@ function Catch_Finish() {
 
 # ENV used by PROW ci
 export CI="openshift"
-export ARTIFACTS_DIR=${ARTIFACT_DIR:-"/tmp/artifacts-che"}
-export NAMESPACE="eclipse-che"
-export DEVWORKSPACE_CONTROLLER_NAMESPACE="devworkspace-controller"
-export OPERATOR_REPO=$(dirname $(dirname $(readlink -f "$0")));
-export HAPPY_PATH_POD_NAME=happy-path-che
-export HAPPY_PATH_DEVFILE='https://gist.githubusercontent.com/l0rd/71a04dd0d8c8e921b16ba2690f7d5a47/raw/d520086e148c359b18c229328824dfefcf85e5ef/spring-petclinic-devfile-v2.0.0.yaml'
+export ARTIFACTS_DIR="/tmp/artifacts"
+export NAMESPACE="devworkspace-controller"
+# Component is defined in Openshift CI job configuration. See: https://github.com/openshift/release/blob/master/ci-operator/config/devfile/devworkspace-operator/devfile-devworkspace-operator-master__v4.yaml#L8
+export CI_COMPONENT="devworkspace-operator"
+# DEVWORKSPACE_OPERATOR env var exposed by Openshift CI in e2e test pod. More info about how images are builded in Openshift CI: https://github.com/openshift/ci-tools/blob/master/TEMPLATES.md#parameters-available-to-templates
+# Dependencies environment are defined here: https://github.com/openshift/release/blob/master/ci-operator/config/devfile/devworkspace-operator/devfile-devworkspace-operator-master__v5.yaml#L36-L38
+
+export DWO_IMG=${DEVWORKSPACE_OPERATOR}
 
 # Pod created by openshift ci don't have user. Using this envs should avoid errors with git user.
 export GIT_COMMITTER_NAME="CI BOT"
@@ -42,129 +44,26 @@ export GIT_COMMITTER_EMAIL="ci_bot@notused.com"
 function getDevWorkspaceOperatorLogs() {
     mkdir -p ${ARTIFACTS_DIR}/devworkspace-operator
     cd ${ARTIFACTS_DIR}/devworkspace-operator
-    for POD in $(oc get pods -o name -n ${DEVWORKSPACE_CONTROLLER_NAMESPACE}); do
-       for CONTAINER in $(oc get -n ${DEVWORKSPACE_CONTROLLER_NAMESPACE} ${POD} -o jsonpath="{.spec.containers[*].name}"); do
+    for POD in $(oc get pods -o name -n ${NAMESPACE}); do
+       for CONTAINER in $(oc get -n ${NAMESPACE} ${POD} -o jsonpath="{.spec.containers[*].name}"); do
             echo ""
             echo "<=========================Getting logs from container $CONTAINER in $POD==================================>"
             echo ""
-            oc logs ${POD} -c ${CONTAINER} -n ${DEVWORKSPACE_CONTROLLER_NAMESPACE} | tee $(echo ${POD}-${CONTAINER}.log | sed 's|pod/||g')
+            oc logs ${POD} -c ${CONTAINER} -n ${NAMESPACE} | tee $(echo ${POD}-${CONTAINER}.log | sed 's|pod/||g')
         done
     done
     echo "======== oc get events ========"
-    oc get events -n ${DEVWORKSPACE_CONTROLLER_NAMESPACE}| tee get_events.log
-
-  mkdir -p ${ARTIFACTS_DIR}
-  /tmp/chectl/bin/chectl server:logs --chenamespace=${NAMESPACE} --directory=${ARTIFACTS_DIR} --telemetry=off
+    oc get events -n ${NAMESPACE}| tee get_events.log
 }
 
-installChectl() {
-  curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && \
-  chmod +x ./kubectl 
-  mv ./kubectl /tmp 
+# For some reason go on PROW force usage vendor folder
+# This workaround is here until we don't figure out cause
+go mod tidy
+go mod vendor
 
-  wget -q https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.7.0-rc.1/openshift-client-linux.tar.gz --no-check-certificate -O - | tar -xz
-  mv oc /tmp
-
-  wget $(curl https://che-incubator.github.io/chectl/download-link/next-linux-x64)
-  tar -xzf chectl-linux-x64.tar.gz
-  mv chectl /tmp
-  /tmp/chectl/bin/chectl --version
-}
-
-deployChe() {
-  cat > /tmp/che-cr-patch.yaml <<EOL
-spec:
-  devWorkspace:
-    enable: true
-    controllerImage: '${DEVWORKSPACE_OPERATOR}'
-  server:
-    customCheProperties:
-      CHE_FACTORY_DEFAULT__PLUGINS: ""
-      CHE_WORKSPACE_DEVFILE_DEFAULT__EDITOR_PLUGINS: ""
-  auth:
-    updateAdminPassword: false
-EOL
-
-  cat /tmp/che-cr-patch.yaml
-
-  /tmp/chectl/bin/chectl server:deploy --che-operator-cr-patch-yaml=/tmp/che-cr-patch.yaml -p openshift --batch --telemetry=off --installer=operator
-}
-
-# Create admin user inside of openshift cluster and login
-function provisionOpenShiftOAuthUser() {
-  oc create secret generic htpass-secret --from-file=htpasswd="${OPERATOR_REPO}"/.github/resources/users.htpasswd -n openshift-config
-  oc apply -f "${OPERATOR_REPO}"/.github/resources/htpasswdProvider.yaml
-  oc adm policy add-cluster-role-to-user cluster-admin user
-
-  echo -e "[INFO] Waiting for htpasswd auth to be working up to 5 minutes"
-  CURRENT_TIME=$(date +%s)
-  ENDTIME=$(($CURRENT_TIME + 300))
-  while [ $(date +%s) -lt $ENDTIME ]; do
-      if oc login -u user -p user --insecure-skip-tls-verify=false; then
-          break
-      fi
-      sleep 10
-  done
-}
-
-startHappyPathTest() {
-  # patch happy-path-che.yaml 
-  ECLIPSE_CHE_URL=http://$(oc get route -n "${NAMESPACE}" che -o jsonpath='{.status.ingress[0].host}')
-  TS_SELENIUM_DEVWORKSPACE_URL="${ECLIPSE_CHE_URL}/#${HAPPY_PATH_DEVFILE}"
-  sed -i "s@CHE_URL@${ECLIPSE_CHE_URL}@g" ${OPERATOR_REPO}/.ci/happy-path-che.yaml
-  sed -i "s@WORKSPACE_ROUTE@${TS_SELENIUM_DEVWORKSPACE_URL}@g" ${OPERATOR_REPO}/.ci/happy-path-che.yaml
-  sed -i "s@CHE-NAMESPACE@${NAMESPACE}@g" ${OPERATOR_REPO}/.ci/happy-path-che.yaml
-  cat ${OPERATOR_REPO}/.ci/happy-path-che.yaml
-
-  oc apply -f ${OPERATOR_REPO}/.ci/happy-path-che.yaml
-  # wait for the pod to start
-  n=0
-  while [ $n -le 120 ]
-  do
-    PHASE=$(oc get pod -n ${NAMESPACE} ${HAPPY_PATH_POD_NAME} \
-        --template='{{ .status.phase }}')
-    if [[ ${PHASE} == "Running" ]]; then
-      echo "[INFO] Happy-path test started succesfully."
-      return
-    fi
-
-    sleep 5
-    n=$(( n+1 ))
-  done
-
-  echo "[ERROR] Failed to start happy-path test."
-  exit 1
-}
-
-
-
-runTest() {
-  deployChe
-
-  startHappyPathTest
-
-  # wait for the test to finish
-  oc logs -n ${NAMESPACE} ${HAPPY_PATH_POD_NAME} -c happy-path-test -f
-
-  # just to sleep
-  sleep 3
-
-  # download the test results
-  mkdir -p /tmp/e2e
-  oc rsync -n ${NAMESPACE} ${HAPPY_PATH_POD_NAME}:/tmp/e2e/report/ /tmp/e2e -c download-reports
-  oc exec -n ${NAMESPACE} ${HAPPY_PATH_POD_NAME} -c download-reports -- touch /tmp/done
-
-  mkdir -p ${ARTIFACTS_DIR}
-  cp -r /tmp/e2e ${ARTIFACTS_DIR}
-
-  EXIT_CODE=$(oc logs -n ${NAMESPACE} ${HAPPY_PATH_POD_NAME} -c happy-path-test | grep EXIT_CODE)
-
-  if [[ ${EXIT_CODE} == "+ EXIT_CODE=1" ]]; then
-    echo "[ERROR] Happy-path test failed."
-    exit 1
-  fi
-}
-
-installChectl
-provisionOpenShiftOAuthUser
-runTest
+make install
+# configure e2e tests not to clean up the resources since it's just for tests instance
+# and we might need to grab data from there
+export CLEAN_UP_AFTER_SUITE="false"
+make test_e2e
+make uninstall

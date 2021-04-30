30f9ec5e1458ae5eb420b202b560d26c17dab183
Angel Misevski <amisevsk@redhat.com>
2021-04-30 15:12:26 -0400

    Use devfile/api "Error" devworkspacePhase instead of hard-coded string
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- controllers/workspace/finalize.go
+++ controllers/workspace/finalize.go
@@ -68,7 +68,7 @@ func (r *DevWorkspaceReconciler) finalize(ctx context.Context, log logr.Logger, 
 	storageProvisioner, err := storage.GetProvisioner(workspace)
 	if err != nil {
 		log.Error(err, "Failed to clean up DevWorkspace storage")
-		failedStatus := currentStatus{phase: "Error"}
+		failedStatus := currentStatus{phase: dw.DevWorkspaceStatusError}
 		failedStatus.setConditionTrue(dw.DevWorkspaceError, err.Error())
 		return r.updateWorkspaceStatus(workspace, r.Log, &failedStatus, reconcile.Result{}, nil)
 	}
@@ -85,7 +85,7 @@ func (r *DevWorkspaceReconciler) finalize(ctx context.Context, log logr.Logger, 
 			return reconcile.Result{RequeueAfter: storageErr.RequeueAfter}, nil
 		case *storage.ProvisioningError:
 			log.Error(storageErr, "Failed to clean up DevWorkspace storage")
-			failedStatus := currentStatus{phase: "Error"}
+			failedStatus := currentStatus{phase: dw.DevWorkspaceStatusError}
 			failedStatus.setConditionTrue(dw.DevWorkspaceError, err.Error())
 			return r.updateWorkspaceStatus(workspace, r.Log, &failedStatus, reconcile.Result{}, nil)
 		default:

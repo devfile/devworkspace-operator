fa7611484fa37dfa1cf377b6e67139eeb50d0ff4
Josh Pinkney <joshpinkney@gmail.com>
2021-08-20 13:39:13 -0400

    Add admissionreviewversions to mutating and validating webhook configs
    Signed-off-by: Josh Pinkney <joshpinkney@gmail.com>

--- webhook/workspace/validating_cfg.go
+++ webhook/workspace/validating_cfg.go
@@ -56,6 +56,7 @@ func buildValidatingWebhookCfg(namespace string) *admregv1.ValidatingWebhookConf
 						},
 					},
 				},
+				AdmissionReviewVersions: []string{"v1beta1", "v1"},
 			},
 		},
 	}

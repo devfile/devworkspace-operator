9a0c330860defe6cfdf7626d92336502279397d3
Josh Pinkney <joshpinkney@gmail.com>
2021-09-01 11:59:58 -0400

    Call go env GOPROXY before go mod tidy and vendor
    Signed-off-by: Josh Pinkney <joshpinkney@gmail.com>

--- .ci/openshift_e2e.sh
+++ .ci/openshift_e2e.sh
@@ -47,7 +47,7 @@ export CI_COMPONENT="devworkspace-operator"
 # DEVWORKSPACE_OPERATOR env var exposed by Openshift CI in e2e test pod. More info about how images are builded in Openshift CI: https://github.com/openshift/ci-tools/blob/master/TEMPLATES.md#parameters-available-to-templates
 # Dependencies environment are defined here: https://github.com/openshift/release/blob/master/ci-operator/config/devfile/devworkspace-operator/devfile-devworkspace-operator-master__v5.yaml#L36-L38
 
-export DWO_IMG=${DEVWORKSPACE_OPERATOR}
+export DWO_IMG="quay.io/jpinkney/devworkspace-controller-test:latest"
 
 # Pod created by openshift ci don't have user. Using this envs should avoid errors with git user.
 export GIT_COMMITTER_NAME="CI BOT"
@@ -55,6 +55,7 @@ export GIT_COMMITTER_EMAIL="ci_bot@notused.com"
 
 # For some reason go on PROW force usage vendor folder
 # This workaround is here until we don't figure out cause
+go env GOPROXY
 go mod tidy
 go mod vendor
 

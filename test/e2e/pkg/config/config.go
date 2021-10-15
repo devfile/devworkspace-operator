5e82c76619c8ea10459d8310df6d7c015e08b416
Angel Misevski <amisevsk@redhat.com>
2021-10-14 20:59:39 -0400

    Add blank line between license header and package
    The updated license headers do not have a blank line between the license
    body and the 'package XXXXX' directive. This results in Go treating the
    license header as package documentation, which is not desired.
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- test/e2e/pkg/config/config.go
+++ test/e2e/pkg/config/config.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+
 package config
 
 import "github.com/devfile/devworkspace-operator/test/e2e/pkg/client"

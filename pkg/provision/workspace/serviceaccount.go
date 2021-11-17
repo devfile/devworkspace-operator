e682bf1f3ca3c62044c037b5828b5d176af8a0e2
Angel Misevski <amisevsk@redhat.com>
2021-11-17 16:27:00 -0500

    Add SCC to workspace ServiceAccount when specified in DevWorkspace.
    Look into 'controller.devfile.io/scc' attribute on DevWorkspaces and
    attempt to add scc listed there to the workspace serviceAccount. This
    requires additional privileges for the DW controller which are not
    provided by default (must be granted explicitly by a cluster-admin)
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- pkg/provision/workspace/serviceaccount.go
+++ pkg/provision/workspace/serviceaccount.go
@@ -16,11 +16,16 @@
 package workspace
 
 import (
+	"fmt"
+
 	dw "github.com/devfile/api/v2/pkg/apis/workspaces/v1alpha2"
 	"github.com/devfile/devworkspace-operator/pkg/constants"
 	"github.com/devfile/devworkspace-operator/pkg/provision/sync"
+	securityv1 "github.com/openshift/api/security/v1"
 	corev1 "k8s.io/api/core/v1"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	"github.com/devfile/devworkspace-operator/pkg/common"
@@ -79,10 +84,60 @@ func SyncServiceAccount(
 		return ServiceAcctProvisioningStatus{ProvisioningStatus: ProvisioningStatus{Err: err}}
 	}
 
+	if workspace.Spec.Template.Attributes.Exists(constants.WorkspaceSCCAttribute) {
+		sccName := workspace.Spec.Template.Attributes.GetString(constants.WorkspaceSCCAttribute, nil)
+		retry, err := addSCCToServiceAccount(specSA.Name, specSA.Namespace, sccName, clusterAPI)
+		if err != nil {
+			return ServiceAcctProvisioningStatus{ProvisioningStatus: ProvisioningStatus{FailStartup: true, Err: err}}
+		}
+		if retry {
+			return ServiceAcctProvisioningStatus{ProvisioningStatus: ProvisioningStatus{Requeue: true}}
+		}
+	}
+
 	return ServiceAcctProvisioningStatus{
 		ProvisioningStatus: ProvisioningStatus{
 			Continue: true,
 		},
 		ServiceAccountName: saName,
 	}
+}
+
+func addSCCToServiceAccount(saName, namespace, sccName string, clusterAPI sync.ClusterAPI) (retry bool, err error) {
+	serviceaccount := fmt.Sprintf("system:serviceaccount:%s:%s", namespace, saName)
+
+	// TODO: Check if we can access the SCC
+
+	scc := &securityv1.SecurityContextConstraints{}
+	if err := clusterAPI.Client.Get(clusterAPI.Ctx, types.NamespacedName{Name: sccName}, scc); err != nil {
+		switch {
+		case k8sErrors.IsUnauthorized(err):
+			return false, fmt.Errorf("operator does not have permissions to get the '%s' SecurityContextConstraint", sccName)
+		case k8sErrors.IsNotFound(err):
+			return false, fmt.Errorf("requested SCC '%s' not found on cluster", sccName)
+		default:
+			return false, err
+		}
+	}
+
+	for _, user := range scc.Users {
+		if user == serviceaccount {
+			// This serviceaccount is already added to the SCC
+			return false, nil
+		}
+	}
+
+	scc.Users = append(scc.Users, serviceaccount)
+	if err := clusterAPI.Client.Update(clusterAPI.Ctx, scc); err != nil {
+		switch {
+		case k8sErrors.IsUnauthorized(err):
+			return false, fmt.Errorf("operator does not have permissions to update the '%s' SecurityContextConstraint", sccName)
+		case k8sErrors.IsConflict(err):
+			return true, nil
+		default:
+			return false, err
+		}
+	}
+
+	return false, nil
 }

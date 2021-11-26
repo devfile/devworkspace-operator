b55e4757a91fabb3c266e2f97192465199b349e2
Angel Misevski <amisevsk@redhat.com>
2021-11-26 00:01:51 +0000

    [release] Bump to 0.11.1 in 0.11.x
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- version/version.go
+++ version/version.go
@@ -17,7 +17,7 @@ package version
 
 var (
 	// Version is the operator version
-	Version = "v0.11.0"
+	Version = "v0.11.1"
 	// Commit is the commit hash corresponding to the code that was built. Can be suffixed with `-dirty`
 	Commit string = "unknown"
 	// BuildTime is the time of build of the binary

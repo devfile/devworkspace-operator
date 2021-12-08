9d71458cb60b537ca503b00b159f2b2e8da61fdd
Josh Pinkney <joshpinkney@gmail.com>
2021-12-08 15:41:15 -0500

    Adjust devworkspacerouting types to not depend on devfile api
    Signed-off-by: Josh Pinkney <joshpinkney@gmail.com>

--- apis/controller/v1alpha1/devworkspacerouting_types.go
+++ apis/controller/v1alpha1/devworkspacerouting_types.go
@@ -16,8 +16,7 @@
 package v1alpha1
 
 import (
-	dw "github.com/devfile/api/v2/pkg/apis/workspaces/v1alpha2"
-	devfileAttr "github.com/devfile/api/v2/pkg/attributes"
+	apiext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -72,12 +71,99 @@ type ExposedEndpoint struct {
 	Url string `json:"url"`
 	// Attributes of the exposed endpoint
 	// +optional
-	Attributes devfileAttr.Attributes `json:"attributes,omitempty"`
+	Attributes Attributes `json:"attributes,omitempty"`
 }
 
-type EndpointList []dw.Endpoint
+type EndpointList []Endpoint
 
 type ExposedEndpointList []ExposedEndpoint
+
+type EndpointExposure string
+
+const (
+	// Endpoint will be exposed on the public network, typically through
+	// a K8S ingress or an OpenShift route
+	PublicEndpointExposure EndpointExposure = "public"
+	// Endpoint will be exposed internally outside of the main devworkspace POD,
+	// typically by K8S services, to be consumed by other elements running
+	// on the same cloud internal network.
+	InternalEndpointExposure EndpointExposure = "internal"
+	// Endpoint will not be exposed and will only be accessible
+	// inside the main devworkspace POD, on a local address.
+	NoneEndpointExposure EndpointExposure = "none"
+)
+
+type EndpointProtocol string
+
+type Attributes map[string]apiext.JSON
+
+type Endpoint struct {
+	// +kubebuilder:validation:Pattern=^[a-z0-9]([-a-z0-9]*[a-z0-9])?$
+	// +kubebuilder:validation:MaxLength=63
+	Name string `json:"name"`
+
+	TargetPort int `json:"targetPort"`
+
+	// Describes how the endpoint should be exposed on the network.
+	//
+	// - `public` means that the endpoint will be exposed on the public network, typically through
+	// a K8S ingress or an OpenShift route.
+	//
+	// - `internal` means that the endpoint will be exposed internally outside of the main devworkspace POD,
+	// typically by K8S services, to be consumed by other elements running
+	// on the same cloud internal network.
+	//
+	// - `none` means that the endpoint will not be exposed and will only be accessible
+	// inside the main devworkspace POD, on a local address.
+	//
+	// Default value is `public`
+	// +optional
+	// +kubebuilder:default=public
+	Exposure EndpointExposure `json:"exposure,omitempty"`
+
+	// Describes the application and transport protocols of the traffic that will go through this endpoint.
+	//
+	// - `http`: Endpoint will have `http` traffic, typically on a TCP connection.
+	// It will be automaticaly promoted to `https` when the `secure` field is set to `true`.
+	//
+	// - `https`: Endpoint will have `https` traffic, typically on a TCP connection.
+	//
+	// - `ws`: Endpoint will have `ws` traffic, typically on a TCP connection.
+	// It will be automaticaly promoted to `wss` when the `secure` field is set to `true`.
+	//
+	// - `wss`: Endpoint will have `wss` traffic, typically on a TCP connection.
+	//
+	// - `tcp`: Endpoint will have traffic on a TCP connection, without specifying an application protocol.
+	//
+	// - `udp`: Endpoint will have traffic on an UDP connection, without specifying an application protocol.
+	//
+	// Default value is `http`
+	// +optional
+	// +kubebuilder:default=http
+	Protocol EndpointProtocol `json:"protocol,omitempty"`
+
+	// Describes whether the endpoint should be secured and protected by some
+	// authentication process. This requires a protocol of `https` or `wss`.
+	// +optional
+	Secure *bool `json:"secure,omitempty"`
+
+	// Path of the endpoint URL
+	// +optional
+	Path string `json:"path,omitempty"`
+
+	// Map of implementation-dependant string-based free-form attributes.
+	//
+	// Examples of Che-specific attributes:
+	//
+	// - cookiesAuthEnabled: "true" / "false",
+	//
+	// - type: "terminal" / "ide" / "ide-dev",
+	// +optional
+	// +kubebuilder:validation:Type=object
+	// +kubebuilder:pruning:PreserveUnknownFields
+	// +kubebuilder:validation:Schemaless
+	Attributes Attributes `json:"attributes,omitempty"`
+}
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 

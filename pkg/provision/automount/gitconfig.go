05d6e73d0cfe7fc2565f0792a1b8689655d69d8e
Angel Misevski <amisevsk@redhat.com>
2022-02-24 16:59:04 -0500

    Refactor git configuration provisioning in automount package
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

---
+++ pkg/provision/automount/gitconfig.go
@@ -0,0 +1,109 @@
+// Copyright (c) 2019-2022 Red Hat, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package automount
+
+import (
+	"fmt"
+
+	"github.com/devfile/devworkspace-operator/pkg/constants"
+	"github.com/devfile/devworkspace-operator/pkg/provision/sync"
+	corev1 "k8s.io/api/core/v1"
+	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// ProvisionGitConfiguration takes care of mounting git credentials and a gitconfig into a devworkspace.
+func ProvisionGitConfiguration(api sync.ClusterAPI, namespace string) (*Resources, error) {
+	credentialsSecrets, tlsConfigMaps, err := getGitResources(api, namespace)
+	if err != nil {
+		return nil, err
+	}
+	if len(credentialsSecrets) == 0 && len(tlsConfigMaps) == 0 {
+		return nil, nil
+	}
+
+	mergedCredentialsSecret, err := mergeGitCredentials(namespace, credentialsSecrets)
+	if err != nil {
+		return nil, &AutoMountError{IsFatal: true, Err: err}
+	}
+
+	credentialsMountPath, err := getCredentialsMountPath(credentialsSecrets)
+	if err != nil {
+		return nil, &AutoMountError{IsFatal: true, Err: err}
+	}
+
+	gitConfigMap, err := constructGitConfig(namespace, credentialsMountPath, tlsConfigMaps)
+	if err != nil {
+		return nil, &AutoMountError{IsFatal: true, Err: err}
+	}
+
+	_, err = sync.SyncObjectWithCluster(mergedCredentialsSecret, api)
+	switch t := err.(type) {
+	case nil:
+		break
+	case *sync.NotInSyncError:
+		return nil, &AutoMountError{IsFatal: false, Err: fmt.Errorf("syncing merged git credentials secret: %w", t)}
+	case *sync.UnrecoverableSyncError:
+		return nil, &AutoMountError{IsFatal: true, Err: fmt.Errorf("failed to sync merged git credentials secret: %w", t.Cause)}
+	default:
+		return nil, &AutoMountError{IsFatal: false, Err: err}
+	}
+
+	_, err = sync.SyncObjectWithCluster(gitConfigMap, api)
+	switch t := err.(type) {
+	case nil:
+		break
+	case *sync.NotInSyncError:
+		return nil, &AutoMountError{IsFatal: false, Err: fmt.Errorf("syncing gitconfig configmap: %w", t)}
+	case *sync.UnrecoverableSyncError:
+		return nil, &AutoMountError{IsFatal: true, Err: fmt.Errorf("failed to sync gitconfig configmap: %w", t.Cause)}
+	default:
+		return nil, &AutoMountError{IsFatal: false, Err: err}
+	}
+
+	resources := flattenAutomountResources([]Resources{
+		getAutomountSecret(credentialsMountPath, constants.DevWorkspaceMountAsSubpath, mergedCredentialsSecret),
+		getAutomountConfigmap("/etc/", constants.DevWorkspaceMountAsSubpath, gitConfigMap),
+	})
+
+	return &resources, nil
+}
+
+func getGitResources(api sync.ClusterAPI, namespace string) (credentialSecrets []corev1.Secret, tlsConfigMaps []corev1.ConfigMap, err error) {
+	credentialsLabelSelector := k8sclient.MatchingLabels{
+		constants.DevWorkspaceGitCredentialLabel: "true",
+	}
+	tlsLabelSelector := k8sclient.MatchingLabels{
+		constants.DevWorkspaceGitTLSLabel: "true",
+	}
+
+	secretList := &corev1.SecretList{}
+	if err := api.Client.List(api.Ctx, secretList, k8sclient.InNamespace(namespace), credentialsLabelSelector); err != nil {
+		return nil, nil, err
+	}
+	var secrets []corev1.Secret
+	if len(secretList.Items) > 0 {
+		secrets = secretList.Items
+	}
+
+	configmapList := &corev1.ConfigMapList{}
+	if err := api.Client.List(api.Ctx, configmapList, k8sclient.InNamespace(namespace), tlsLabelSelector); err != nil {
+		return nil, nil, err
+	}
+	var configmaps []corev1.ConfigMap
+	if len(configmapList.Items) > 0 {
+		configmaps = configmapList.Items
+	}
+
+	return secrets, configmaps, nil
+}
---
+++ pkg/provision/automount/templates.go
@@ -0,0 +1,147 @@
+// Copyright (c) 2019-2022 Red Hat, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package automount
+
+import (
+	"fmt"
+	"path"
+	"strings"
+
+	"github.com/devfile/devworkspace-operator/pkg/constants"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const gitTLSHostKey = "host"
+const gitTLSCertificateKey = "certificate"
+
+const gitConfigName = "gitconfig"
+const gitConfigLocation = "/etc/" + gitConfigName
+const gitCredentialsConfigMapName = "devworkspace-gitconfig"
+
+const gitCredentialsSecretKey = "credentials"
+const gitCredentialsSecretName = "devworkspace-merged-git-credentials"
+
+const credentialTemplate = `[credential]
+    helper = store --file %s
+`
+
+const gitServerTemplate = `[http "%s"]
+    sslCAInfo = %s
+`
+
+const defaultGitServerTemplate = `[http]
+    sslCAInfo = %s
+`
+
+func constructGitConfig(namespace, credentialMountPath string, certificatesConfigMaps []corev1.ConfigMap) (*corev1.ConfigMap, error) {
+	var configSettings []string
+	if credentialMountPath != "" {
+		configSettings = append(configSettings, fmt.Sprintf(credentialTemplate, path.Join(credentialMountPath, gitCredentialsSecretKey)))
+	}
+
+	defaultTLSFound := false
+	for _, cm := range certificatesConfigMaps {
+		if _, certFound := cm.Data[gitTLSCertificateKey]; !certFound {
+			return nil, fmt.Errorf("could not find certificate field in configmap %s", cm.Name)
+		}
+
+		mountPath := cm.Annotations[constants.DevWorkspaceMountPathAnnotation]
+		if mountPath == "" {
+			mountPath = fmt.Sprintf("/etc/config/%s", cm.Name)
+		}
+		certificatePath := path.Join(mountPath, gitTLSCertificateKey)
+
+		host, hostFound := cm.Data[gitTLSHostKey]
+		if !hostFound {
+			if defaultTLSFound {
+				return nil, fmt.Errorf("multiple git tls credentials do not have host specified")
+			}
+			configSettings = append(configSettings, fmt.Sprintf(defaultGitServerTemplate, certificatePath))
+			defaultTLSFound = true
+		} else {
+			configSettings = append(configSettings, fmt.Sprintf(gitServerTemplate, host, certificatePath))
+		}
+	}
+
+	gitConfig := strings.Join(configSettings, "\n")
+
+	gitConfigMap := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      gitCredentialsConfigMapName,
+			Namespace: namespace,
+			Labels: map[string]string{
+				"app.kubernetes.io/defaultName":         "git-config-secret",
+				"app.kubernetes.io/part-of":             "devworkspace-operator",
+				"controller.devfile.io/watch-configmap": "true",
+			},
+		},
+		Data: map[string]string{
+			gitConfigName: gitConfig,
+		},
+	}
+
+	return gitConfigMap, nil
+}
+
+func mergeGitCredentials(namespace string, credentialSecrets []corev1.Secret) (*corev1.Secret, error) {
+	var allCredentials []string
+	for _, credentialSecret := range credentialSecrets {
+		credential, found := credentialSecret.Data[gitCredentialsSecretKey]
+		if !found {
+			return nil, fmt.Errorf("git-credentials secret %s does not contain data in key %s", credentialSecret.Name, gitCredentialsSecretKey)
+		}
+		allCredentials = append(allCredentials, string(credential))
+	}
+	mergedCredentials := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      gitCredentialsSecretName,
+			Namespace: namespace,
+			Labels: map[string]string{
+				"app.kubernetes.io/defaultName":      "git-config-secret",
+				"app.kubernetes.io/part-of":          "devworkspace-operator",
+				"controller.devfile.io/watch-secret": "true",
+			},
+		},
+		Data: map[string][]byte{
+			gitCredentialsSecretKey: []byte(strings.Join(allCredentials, "\n")),
+		},
+		Type: corev1.SecretTypeOpaque,
+	}
+	return mergedCredentials, nil
+}
+
+// getCredentialsMountPath returns the mount path to be used by all git credentials secrets. If no secrets define a mountPath,
+// the root path ('/credentials') is used. If secrets define conflicting mountPaths, an error is returned and represents an invalid
+// configuration. If any secret defines a mountPath, that mountPath overrides the mountPath for all secrets that do not
+// define a mountPath. If there are no credentials secrets, the empty string is returned
+func getCredentialsMountPath(secrets []corev1.Secret) (string, error) {
+	if len(secrets) == 0 {
+		return "", nil
+	}
+	mountPath := ""
+	for _, secret := range secrets {
+		secretMountPath := secret.Annotations[constants.DevWorkspaceMountPathAnnotation]
+		if secretMountPath != "" {
+			if mountPath != "" && secretMountPath != mountPath {
+				return "", fmt.Errorf("auto-mounted git credentials have conflicting mountPaths: %s, %s", mountPath, secretMountPath)
+			}
+			mountPath = secretMountPath
+		}
+	}
+	if mountPath == "" {
+		mountPath = "/"
+	}
+	return mountPath, nil
+}

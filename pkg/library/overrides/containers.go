b18b38698195d75433593c6e45424aed363dbee7
Angel Misevski <amisevsk@redhat.com>
2022-11-03 12:19:13 -0400

    Support defining strategic patch directives in override attributes
    Add support for Kubernetes strategic merge directives, allowing more
    control over pod/container overrides. For example, the attribute
    
        pod-overrides:
          spec:
            securityContext:
              runAsUser: 1001
              $patch: replace
    
    can be used to overwrite the default securityContext for a pod -- if the
    default pod SecurityContext is configured to be
    
        podSecurityContext:
          fsGroup: 2000
          runAsGroup: 3000
          runAsUser: 1000
    
    using $patch: replace will result in the pod SecurityContext
    
        podSecurityContext:
          runAsUser: 1001
    
    instead of
    
        podSecurityContext:
          fsGroup: 2000
          runAsGroup: 3000
          runAsUser: 1001
    
    To achieve this, we use the raw patch as defined in the overrides
    attribute rather than deserializing and reserializing it.
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- pkg/library/overrides/containers.go
+++ pkg/library/overrides/containers.go
@@ -33,19 +33,18 @@ func ApplyContainerOverrides(component *dw.Component, container *corev1.Containe
 	if err := component.Attributes.GetInto(constants.ContainerOverridesAttribute, override); err != nil {
 		return nil, fmt.Errorf("failed to parse %s attribute on component %s: %w", constants.ContainerOverridesAttribute, component.Name, err)
 	}
-	override = restrictContainerOverride(override)
-
-	overrideBytes, err := json.Marshal(override)
-	if err != nil {
-		return nil, fmt.Errorf("error applying container overrides: %w", err)
+	if err := restrictContainerOverride(override); err != nil {
+		return nil, fmt.Errorf("failed to parse %s attribute on component %s: %w", constants.ContainerOverridesAttribute, component.Name, err)
 	}
+
+	overrideJSON := component.Attributes[constants.ContainerOverridesAttribute]
 
 	originalBytes, err := json.Marshal(container)
 	if err != nil {
 		return nil, fmt.Errorf("failed to marshal container to yaml: %w", err)
 	}
 
-	patchedBytes, err := strategicpatch.StrategicMergePatch(originalBytes, overrideBytes, &corev1.Container{})
+	patchedBytes, err := strategicpatch.StrategicMergePatch(originalBytes, overrideJSON.Raw, &corev1.Container{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to apply container overrides: %w", err)
 	}
@@ -54,24 +53,41 @@ func ApplyContainerOverrides(component *dw.Component, container *corev1.Containe
 	if err := json.Unmarshal(patchedBytes, patched); err != nil {
 		return nil, fmt.Errorf("error applying container overrides: %w", err)
 	}
-	// Applying the patch will overwrite the container's name as corev1.Container.Name
+	// Applying the patch will overwrite the container's name and image as corev1.Container.Name
 	// does not have the omitempty json tag.
 	patched.Name = container.Name
+	patched.Image = container.Image
 	return patched, nil
 }
 
 // restrictContainerOverride unsets fields on a container that should not be
 // considered for container overrides. These fields are generally available to
 // set as fields on the container component itself.
-func restrictContainerOverride(override *corev1.Container) *corev1.Container {
-	result := override.DeepCopy()
-	result.Name = ""
-	result.Image = ""
-	result.Command = nil
-	result.Args = nil
-	result.Ports = nil
-	result.VolumeMounts = nil
-	result.Env = nil
-
-	return result
+func restrictContainerOverride(override *corev1.Container) error {
+	invalidField := ""
+	if override.Name != "" {
+		invalidField = "name"
+	}
+	if override.Image != "" {
+		invalidField = "image"
+	}
+	if override.Command != nil {
+		invalidField = "command"
+	}
+	if override.Args != nil {
+		invalidField = "args"
+	}
+	if override.Ports != nil {
+		invalidField = "ports"
+	}
+	if override.VolumeMounts != nil {
+		invalidField = "volumeMounts"
+	}
+	if override.Env != nil {
+		invalidField = "env"
+	}
+	if invalidField != "" {
+		return fmt.Errorf("cannot use container-overrides to override container %s", invalidField)
+	}
+	return nil
 }

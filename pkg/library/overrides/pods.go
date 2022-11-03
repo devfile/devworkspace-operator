00414af86fbdfeb2776990d2c538107ed9221a37
Angel Misevski <amisevsk@redhat.com>
2022-11-03 12:19:13 -0400

    Support defining strategic patch directives in override attributes
    Add support for Kubernetes strategic merge directives, allowing more
    control over pod/container overrides. For example, the attribute
    
        pod-overrides:
          spec:
            securityContext:
              runAsUser: 1001
              $patch: replace
    
    can be used to overwrite the default securityContext for a pod -- if the
    default pod SecurityContext is configured to be
    
        podSecurityContext:
          fsGroup: 2000
          runAsGroup: 3000
          runAsUser: 1000
    
    using $patch: replace will result in the pod SecurityContext
    
        podSecurityContext:
          runAsUser: 1001
    
    instead of
    
        podSecurityContext:
          fsGroup: 2000
          runAsGroup: 3000
          runAsUser: 1001
    
    To achieve this, we use the raw patch as defined in the overrides
    attribute rather than deserializing and reserializing it.
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- pkg/library/overrides/pods.go
+++ pkg/library/overrides/pods.go
@@ -18,6 +18,7 @@ import (
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	apiext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/util/json"
 	"k8s.io/apimachinery/pkg/util/strategicpatch"
 
@@ -50,17 +51,15 @@ func ApplyPodOverrides(workspace *common.DevWorkspaceWithConfig, deployment *app
 	// will be interpreted as "delete all containers" as the serialized patch will include "containers": null.
 	// To avoid this, save the original containers and reset them at the end.
 	originalContainers := patched.Spec.Template.Spec.Containers
+	// Save fields we do not allow to be configured in pod-overrides
+	originalInitContainers := patched.Spec.Template.Spec.InitContainers
+	originalVolumes := patched.Spec.Template.Spec.Volumes
 	patchedTemplateBytes, err := json.Marshal(patched.Spec.Template)
 	if err != nil {
 		return nil, fmt.Errorf("failed to marshal deployment to yaml: %w", err)
 	}
 	for _, override := range overrides {
-		patchBytes, err := json.Marshal(override)
-		if err != nil {
-			return nil, fmt.Errorf("error applying pod overrides: %w", err)
-		}
-
-		patchedTemplateBytes, err = strategicpatch.StrategicMergePatch(patchedTemplateBytes, patchBytes, &corev1.PodTemplateSpec{})
+		patchedTemplateBytes, err = strategicpatch.StrategicMergePatch(patchedTemplateBytes, override.Raw, &corev1.PodTemplateSpec{})
 		if err != nil {
 			return nil, fmt.Errorf("error applying pod overrides: %w", err)
 		}
@@ -72,6 +71,8 @@ func ApplyPodOverrides(workspace *common.DevWorkspaceWithConfig, deployment *app
 	}
 	patched.Spec.Template = patchedPodSpecTemplate
 	patched.Spec.Template.Spec.Containers = originalContainers
+	patched.Spec.Template.Spec.InitContainers = originalInitContainers
+	patched.Spec.Template.Spec.Volumes = originalVolumes
 	return patched, nil
 }
 
@@ -79,21 +80,28 @@ func ApplyPodOverrides(workspace *common.DevWorkspaceWithConfig, deployment *app
 // present in the DevWorkspace. The order of elements is
 // 1. Pod overrides defined on Container components, in the order they appear in the DevWorkspace
 // 2. Pod overrides defined in the global attributes field (.spec.template.attributes)
-func getPodOverrides(workspace *common.DevWorkspaceWithConfig) ([]corev1.PodTemplateSpec, error) {
-	var allOverrides []corev1.PodTemplateSpec
+func getPodOverrides(workspace *common.DevWorkspaceWithConfig) ([]apiext.JSON, error) {
+	var allOverrides []apiext.JSON
 
 	for _, component := range workspace.Spec.Template.Components {
 		if component.Attributes.Exists(constants.PodOverridesAttribute) {
 			override := corev1.PodTemplateSpec{}
-			err := component.Attributes.GetInto(constants.PodOverridesAttribute, &override)
-			if err != nil {
+			// Check format of pod-overrides to detect errors early
+			if err := component.Attributes.GetInto(constants.PodOverridesAttribute, &override); err != nil {
 				return nil, fmt.Errorf("failed to parse %s attribute on component %s: %w", constants.PodOverridesAttribute, component.Name, err)
 			}
-			// Do not allow overriding containers
-			override.Spec.Containers = nil
-			override.Spec.InitContainers = nil
-			override.Spec.Volumes = nil
-			allOverrides = append(allOverrides, override)
+			// Do not allow overriding containers or volumes
+			if override.Spec.Containers != nil {
+				return nil, fmt.Errorf("cannot use pod-overrides to override pod containers (component %s)", component.Name)
+			}
+			if override.Spec.InitContainers != nil {
+				return nil, fmt.Errorf("cannot use pod-overrides to override pod initContainers (component %s)", component.Name)
+			}
+			if override.Spec.Volumes != nil {
+				return nil, fmt.Errorf("cannot use pod-overrides to override pod volumes (component %s)", component.Name)
+			}
+			patchData := component.Attributes[constants.PodOverridesAttribute]
+			allOverrides = append(allOverrides, patchData)
 		}
 	}
 	if workspace.Spec.Template.Attributes.Exists(constants.PodOverridesAttribute) {
@@ -103,10 +111,17 @@ func getPodOverrides(workspace *common.DevWorkspaceWithConfig) ([]corev1.PodTemp
 			return nil, fmt.Errorf("failed to parse %s attribute for workspace: %w", constants.PodOverridesAttribute, err)
 		}
 		// Do not allow overriding containers or volumes
-		override.Spec.Containers = nil
-		override.Spec.InitContainers = nil
-		override.Spec.Volumes = nil
-		allOverrides = append(allOverrides, override)
+		if override.Spec.Containers != nil {
+			return nil, fmt.Errorf("cannot use pod-overrides to override pod containers")
+		}
+		if override.Spec.InitContainers != nil {
+			return nil, fmt.Errorf("cannot use pod-overrides to override pod initContainers")
+		}
+		if override.Spec.Volumes != nil {
+			return nil, fmt.Errorf("cannot use pod-overrides to override pod volumes")
+		}
+		patchData := workspace.Spec.Template.Attributes[constants.PodOverridesAttribute]
+		allOverrides = append(allOverrides, patchData)
 	}
 	return allOverrides, nil
 }

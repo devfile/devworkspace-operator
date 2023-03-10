99e8f0eb39cf9651f863e734551dd1e3cb5f2f0f
Angel Misevski <amisevsk@redhat.com>
2023-03-10 13:49:43 -0500

    Add package for handling controller errors in a unified way
    Add package pkg/dwerrors (named such to avoid having to alias it every
    time its imported). This package collects error functionality that is
    duplicated multiple times throughout the library and provision codebase,
    representing 3 types of errors common in DWO:
    
    * FailError (aka ProvisioningError) - an error that should cause
      workspace start to fail
    * RetryError (aka NotReadyError) - an error that indicates that
      the current reconcile should stop and be requeued
    * WarningError - an error indicating that there are warnings to show to
      the user but reconcile can otherwise continue
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

---
+++ pkg/dwerrors/errors.go
@@ -0,0 +1,82 @@
+// Copyright (c) 2019-2023 Red Hat, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dwerrors
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/devfile/devworkspace-operator/pkg/provision/sync"
+)
+
+type RetryError struct {
+	// Err is the underlying error causing the problem. If nil, it is not included in the output of Error()
+	Err error
+	// Message is a user-friendly string explaining why the error occurred
+	Message string
+	// RequeueAfter represents how long we should wait before requeueing a reconcile. If unspecified,
+	// reconcile is requeued immediately.
+	RequeueAfter time.Duration
+}
+
+func (e *RetryError) Error() string {
+	if e.Err != nil {
+		return fmt.Sprintf("%s: %s", e.Message, e.Err)
+	}
+	return e.Message
+}
+
+func (e *RetryError) Unwrap() error {
+	return e.Err
+}
+
+type FailError struct {
+	// Err is the underlying error causing the problem. If nil, it is not included in the output of Error()
+	Err error
+	// Message is a user-friendly string explaining why the error occurred
+	Message string
+}
+
+func (e *FailError) Error() string {
+	if e.Err != nil {
+		return fmt.Sprintf("%s: %s", e.Message, e.Err)
+	}
+	return e.Message
+}
+
+func (e *FailError) Unwrap() error {
+	return e.Err
+}
+
+type WarningError struct {
+	// Message is a user-friendly string explaining the warning present
+	Message string
+}
+
+func (e *WarningError) Error() string {
+	return e.Message
+}
+
+func WrapSyncError(err error) error {
+	switch syncErr := err.(type) {
+	case *sync.NotInSyncError:
+		return &RetryError{Err: syncErr}
+	case *sync.UnrecoverableSyncError:
+		return &FailError{Err: syncErr}
+	case *sync.WarningError:
+		return &WarningError{Message: syncErr.Error()}
+	default:
+		return err
+	}
+}

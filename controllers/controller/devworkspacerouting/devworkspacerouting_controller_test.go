b512c88f908d08558da6ee9750814c08714f1a4a
Angel Misevski <amisevsk@redhat.com>
2023-10-27 09:54:32 -0400

    Drop invalid routing controller test
    Drop devworkspacerouting reconciler test for "gets preparing status", as
    this test relied on a bug (403 forbidden responses to API calls were
    ignored) in the controller.
    
    Since there's no good way to create a 'preparing' DevWorkspaceRouting,
    we can't test this specific function.
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- controllers/controller/devworkspacerouting/devworkspacerouting_controller_test.go
+++ controllers/controller/devworkspacerouting/devworkspacerouting_controller_test.go
@@ -32,29 +32,6 @@ import (
 
 var _ = Describe("DevWorkspaceRouting Controller", func() {
 	Context("Basic DevWorkspaceRouting Tests", func() {
-		It("Gets Preparing status", func() {
-			By("Creating a new DevWorkspaceRouting object")
-			dwrNamespacedName := namespacedName(devWorkspaceRoutingName, testNamespace)
-			createdDWR := createPreparingDWR(testWorkspaceID, devWorkspaceRoutingName)
-			Eventually(func() bool {
-				err := k8sClient.Get(ctx, dwrNamespacedName, createdDWR)
-				return err == nil
-			}, timeout, interval).Should(BeTrue(), "DevWorkspaceRouting should exist in cluster")
-
-			By("Checking DevWorkspaceRouting Status is updated to preparing")
-			Eventually(func() (phase controllerv1alpha1.DevWorkspaceRoutingPhase, err error) {
-				if err := k8sClient.Get(ctx, dwrNamespacedName, createdDWR); err != nil {
-					return "", err
-				}
-				return createdDWR.Status.Phase, nil
-			}, timeout, interval).Should(Equal(controllerv1alpha1.RoutingPreparing), "DevWorkspaceRouting should have Preparing phase")
-
-			Expect(createdDWR.Status.Message).ShouldNot(BeNil(), "Status message should be set for DevWorkspaceRoutings in Preparing phase")
-
-			deleteDevWorkspaceRouting(devWorkspaceRoutingName)
-			// Services and Ingresses aren't created since the DWR was stuck in preparing phase, no need to clean them up
-		})
-
 		It("Gets Ready Status on OpenShift", func() {
 			By("Setting infrastructure to OpenShift")
 			infrastructure.InitializeForTesting(infrastructure.OpenShiftv4)

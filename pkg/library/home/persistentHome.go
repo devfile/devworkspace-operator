82e1666944a35afd421448d8e5cca2b2bf5d2bf2
David Kwon <dakwon@redhat.com>
2024-04-16 18:40:12 -0400

    Provide initcontainer to set up persistent home
    Signed-off-by: David Kwon <dakwon@redhat.com>

--- pkg/library/home/persistentHome.go
+++ pkg/library/home/persistentHome.go
@@ -27,6 +27,24 @@ import (
 	"github.com/devfile/devworkspace-operator/pkg/constants"
 )
 
+const initScript = `(echo "Checking for stow command"
+STOW_COMPLETE=/home/user/.stow_completed
+if command -v stow &> /dev/null; then
+  if  [ ! -f $STOW_COMPLETE ]; then
+    echo "Running stow command"
+    stow . -t /home/user/ -d /home/tooling/ --no-folding -v 2 > /home/user/.stow.log 2>&1
+    cp -n /home/tooling/.viminfo /home/user/.viminfo
+    cp -n /home/tooling/.bashrc /home/user/.bashrc
+    cp -n /home/tooling/.bash_profile /home/user/.bash_profile
+    touch $STOW_COMPLETE
+  else
+    echo "Stow command already run. If you wish to re-run it, delete $STOW_COMPLETE from the persistent volume and restart the workspace."
+  fi
+else
+  echo "Stow command not found"
+fi) || true
+`
+
 // Returns a modified copy of the given DevWorkspace's Template Spec which contains an additional
 // Devfile volume 'persistentHome' that is mounted  to `/home/user/` for every container component defined in the DevWorkspace.
 // An error is returned if the addition of the 'persistentHome' volume would result
@@ -42,6 +60,13 @@ func AddPersistentHomeVolume(workspace *common.DevWorkspaceWithConfig) (*v1alpha
 	homeVolumeMount := v1alpha2.VolumeMount{
 		Name: constants.HomeVolumeName,
 		Path: constants.HomeUserDirectory,
+	}
+
+	if workspace.Config.Workspace.PersistUserHome.DisableInitContainer == nil || !*workspace.Config.Workspace.PersistUserHome.DisableInitContainer {
+		err := addInitContainer(dwTemplateSpecCopy)
+		if err != nil {
+			return nil, fmt.Errorf("failed to add init container for home persistence setup: %w", err)
+		}
 	}
 
 	dwTemplateSpecCopy.Components = append(dwTemplateSpecCopy.Components, homeVolume)
@@ -94,4 +119,123 @@ func NeedsPersistentHomeDirectory(workspace *common.DevWorkspaceWithConfig) bool
 func storageStrategySupportsPersistentHome(workspace *common.DevWorkspaceWithConfig) bool {
 	storageClass := workspace.Spec.Template.Attributes.GetString(constants.DevWorkspaceStorageTypeAttribute, nil)
 	return storageClass != constants.EphemeralStorageClassType
+}
+
+func addInitContainer(dwTemplateSpec *v1alpha2.DevWorkspaceTemplateSpec) error {
+
+	if initComponentExists(dwTemplateSpec) {
+		return fmt.Errorf("component named %s already exists in the devworkspace", constants.HomeInitComponentName)
+	}
+
+	if initCommandExists(dwTemplateSpec) {
+		return fmt.Errorf("command with id %s already exists in the devworkspace", constants.HomeInitEventId)
+	}
+
+	if initEventExists(dwTemplateSpec) {
+		return fmt.Errorf("event with id %s already exists in the devworkspace", constants.HomeInitEventId)
+	}
+
+	initContainer := inferInitContainer(dwTemplateSpec)
+	if initContainer == nil {
+		return fmt.Errorf("cannot infer initcontainer for home persistence setup")
+	}
+
+	addInitContainerComponent(dwTemplateSpec, initContainer)
+
+	if dwTemplateSpec.Commands == nil {
+		dwTemplateSpec.Commands = []v1alpha2.Command{}
+	}
+
+	if dwTemplateSpec.Events == nil {
+		dwTemplateSpec.Events = &v1alpha2.Events{}
+	}
+
+	dwTemplateSpec.Commands = append(dwTemplateSpec.Commands, v1alpha2.Command{
+		Id: constants.HomeInitEventId,
+		CommandUnion: v1alpha2.CommandUnion{
+			Apply: &v1alpha2.ApplyCommand{
+				Component: constants.HomeInitComponentName,
+			},
+		},
+	})
+
+	dwTemplateSpec.Events.PreStart = append(dwTemplateSpec.Events.PreStart, constants.HomeInitEventId)
+
+	return nil
+}
+
+func initComponentExists(dwTemplateSpec *v1alpha2.DevWorkspaceTemplateSpec) bool {
+	if dwTemplateSpec.Components == nil {
+		return false
+	}
+	for _, component := range dwTemplateSpec.Components {
+		if component.Name == constants.HomeInitComponentName {
+			return true
+		}
+	}
+	return false
+
+}
+
+func initCommandExists(dwTemplateSpec *v1alpha2.DevWorkspaceTemplateSpec) bool {
+	if dwTemplateSpec.Commands == nil {
+		return false
+	}
+	for _, command := range dwTemplateSpec.Commands {
+		if command.Id == constants.HomeInitEventId {
+			return true
+		}
+	}
+	return false
+}
+
+func initEventExists(dwTemplateSpec *v1alpha2.DevWorkspaceTemplateSpec) bool {
+	if dwTemplateSpec.Events == nil {
+		return false
+	}
+	for _, event := range dwTemplateSpec.Events.PreStart {
+		if event == constants.HomeInitEventId {
+			return true
+		}
+	}
+	return false
+
+}
+
+func addInitContainerComponent(dwTemplateSpec *v1alpha2.DevWorkspaceTemplateSpec, initContainer *v1alpha2.Container) {
+	initComponent := v1alpha2.Component{
+		Name: constants.HomeInitComponentName,
+		ComponentUnion: v1alpha2.ComponentUnion{
+			Container: &v1alpha2.ContainerComponent{
+				Container: *initContainer,
+			},
+		},
+	}
+	dwTemplateSpec.Components = append(dwTemplateSpec.Components, initComponent)
+}
+
+func inferInitContainer(dwTemplateSpec *v1alpha2.DevWorkspaceTemplateSpec) *v1alpha2.Container {
+	var nonImportedComponent v1alpha2.Component
+	for _, component := range dwTemplateSpec.Components {
+		if component.Container == nil {
+			continue
+		}
+
+		pluginSource := component.Attributes.GetString(constants.PluginSourceAttribute, nil)
+		if pluginSource == "" || pluginSource == "parent" {
+			// First non-imported container component is selected
+			nonImportedComponent = component
+			break
+		}
+	}
+
+	if nonImportedComponent.Name != "" {
+		image := nonImportedComponent.Container.Image
+		return &v1alpha2.Container{
+			Image:   image,
+			Command: []string{"/bin/sh", "-c"},
+			Args:    []string{initScript},
+		}
+	}
+	return nil
 }

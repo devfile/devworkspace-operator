0dddc52ffc7691b4dbbd274196863ad1c87bd54f
Oleksii Kurinnyi <okurinny@redhat.com>
2025-07-28 13:01:36 +0300

    fixup! fixup! fixup! fix: fall back to the original behaviour if timeout is disabled
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>

--- pkg/library/lifecycle/poststart.go
+++ pkg/library/lifecycle/poststart.go
@@ -81,6 +81,78 @@ func AddPostStartLifecycleHooks(wksp *dw.DevWorkspaceTemplateSpec, containers []
 	return nil
 }
 
+// processCommandsForPostStart processes a list of DevWorkspace commands
+// and generates a corev1.LifecycleHandler for the PostStart lifecycle hook.
+func processCommandsForPostStart(commands []dw.Command, postStartTimeout *int32) (*corev1.LifecycleHandler, error) {
+	if postStartTimeout == nil || *postStartTimeout == 0 {
+		// use the fallback if no timeout propagated
+		return processCommandsWithoutTimeoutFallback(commands)
+	}
+
+	originalUserScript, err := buildUserScript(commands)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build aggregated user script: %w", err)
+	}
+
+	// The user script needs 'set -e' to ensure it exits on error.
+	// This script is then passed to `sh -c '...'`, so single quotes within it must be escaped.
+	scriptToExecute := "set -e\n" + originalUserScript
+	escapedUserScriptForTimeoutWrapper := strings.ReplaceAll(scriptToExecute, "'", `'\''`)
+
+	fullScriptWithTimeout := generateScriptWithTimeout(escapedUserScriptForTimeoutWrapper, *postStartTimeout)
+
+	finalScriptForHook := fmt.Sprintf(redirectOutputFmt, fullScriptWithTimeout)
+
+	handler := &corev1.LifecycleHandler{
+		Exec: &corev1.ExecAction{
+			Command: []string{
+				"/bin/sh",
+				"-c",
+				finalScriptForHook,
+			},
+		},
+	}
+	return handler, nil
+}
+
+// processCommandsWithoutTimeoutFallback builds a lifecycle handler that runs the provided command(s)
+// The command has the format
+//
+// exec:
+//
+//	command:
+//	  - "/bin/sh"
+//	  - "-c"
+//	  - |
+//	    cd <workingDir>
+//	    <commandline>
+func processCommandsWithoutTimeoutFallback(commands []dw.Command) (*corev1.LifecycleHandler, error) {
+	var dwCommands []string
+	for _, command := range commands {
+		execCmd := command.Exec
+		if len(execCmd.Env) > 0 {
+			return nil, fmt.Errorf("env vars in postStart command %s are unsupported", command.Id)
+		}
+		if execCmd.WorkingDir != "" {
+			dwCommands = append(dwCommands, fmt.Sprintf("cd %s", execCmd.WorkingDir))
+		}
+		dwCommands = append(dwCommands, execCmd.CommandLine)
+	}
+
+	joinedCommands := strings.Join(dwCommands, "\n")
+
+	handler := &corev1.LifecycleHandler{
+		Exec: &corev1.ExecAction{
+			Command: []string{
+				"/bin/sh",
+				"-c",
+				fmt.Sprintf(noTimeoutRedirectOutputFmt, joinedCommands),
+			},
+		},
+	}
+	return handler, nil
+}
+
 // buildUserScript takes a list of DevWorkspace commands and constructs a single
 // shell script string that executes them sequentially.
 func buildUserScript(commands []dw.Command) (string, error) {
@@ -155,76 +227,4 @@ fi
 
 exit $exit_code
 `, timeoutSeconds, escapedUserScript)
-}
-
-// processCommandsForPostStart processes a list of DevWorkspace commands
-// and generates a corev1.LifecycleHandler for the PostStart lifecycle hook.
-func processCommandsForPostStart(commands []dw.Command, postStartTimeout *int32) (*corev1.LifecycleHandler, error) {
-	if postStartTimeout == nil || *postStartTimeout == 0 {
-		// use the fallback if no timeout propagated
-		return processCommandsWithoutTimeoutFallback(commands)
-	}
-
-	originalUserScript, err := buildUserScript(commands)
-	if err != nil {
-		return nil, fmt.Errorf("failed to build aggregated user script: %w", err)
-	}
-
-	// The user script needs 'set -e' to ensure it exits on error.
-	// This script is then passed to `sh -c '...'`, so single quotes within it must be escaped.
-	scriptToExecute := "set -e\n" + originalUserScript
-	escapedUserScriptForTimeoutWrapper := strings.ReplaceAll(scriptToExecute, "'", `'\''`)
-
-	fullScriptWithTimeout := generateScriptWithTimeout(escapedUserScriptForTimeoutWrapper, *postStartTimeout)
-
-	finalScriptForHook := fmt.Sprintf(redirectOutputFmt, fullScriptWithTimeout)
-
-	handler := &corev1.LifecycleHandler{
-		Exec: &corev1.ExecAction{
-			Command: []string{
-				"/bin/sh",
-				"-c",
-				finalScriptForHook,
-			},
-		},
-	}
-	return handler, nil
-}
-
-// processCommandsForPostStart builds a lifecycle handler that runs the provided command(s)
-// The command has the format
-//
-// exec:
-//
-//	command:
-//	  - "/bin/sh"
-//	  - "-c"
-//	  - |
-//	    cd <workingDir>
-//	    <commandline>
-func processCommandsWithoutTimeoutFallback(commands []dw.Command) (*corev1.LifecycleHandler, error) {
-	var dwCommands []string
-	for _, command := range commands {
-		execCmd := command.Exec
-		if len(execCmd.Env) > 0 {
-			return nil, fmt.Errorf("env vars in postStart command %s are unsupported", command.Id)
-		}
-		if execCmd.WorkingDir != "" {
-			dwCommands = append(dwCommands, fmt.Sprintf("cd %s", execCmd.WorkingDir))
-		}
-		dwCommands = append(dwCommands, execCmd.CommandLine)
-	}
-
-	joinedCommands := strings.Join(dwCommands, "\n")
-
-	handler := &corev1.LifecycleHandler{
-		Exec: &corev1.ExecAction{
-			Command: []string{
-				"/bin/sh",
-				"-c",
-				fmt.Sprintf(noTimeoutRedirectOutputFmt, joinedCommands),
-			},
-		},
-	}
-	return handler, nil
 }

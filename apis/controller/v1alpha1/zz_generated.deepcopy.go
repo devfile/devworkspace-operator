2d2ad37a43d0417dd180592c76668f71071045a4
Oleksii Kurinnyi <okurinny@redhat.com>
2025-08-04 09:12:54 +0000

    fixup! fix: change type `*int32` to `string`
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>

--- apis/controller/v1alpha1/zz_generated.deepcopy.go
+++ apis/controller/v1alpha1/zz_generated.deepcopy.go
@@ -21,7 +21,7 @@ package v1alpha1
 
 import (
 	"github.com/devfile/api/v2/pkg/apis/workspaces/v1alpha2"
-	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/core/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 )
 
@@ -787,11 +787,6 @@ func (in *WorkspaceConfig) DeepCopyInto(out *WorkspaceConfig) {
 		in, out := &in.CleanupCronJob, &out.CleanupCronJob
 		*out = new(CleanupCronJobConfig)
 		(*in).DeepCopyInto(*out)
-	}
-	if in.PostStartTimeout != nil {
-		in, out := &in.PostStartTimeout, &out.PostStartTimeout
-		*out = new(int32)
-		**out = **in
 	}
 }
 

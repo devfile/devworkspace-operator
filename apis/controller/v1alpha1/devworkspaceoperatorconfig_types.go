9c87c0ed95b84a259882cc7c81eff5bfd411678b
Oleksii Kurinnyi <okurinny@redhat.com>
2025-10-30 15:27:19 +0200

    feat: configure init containers
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>

--- apis/controller/v1alpha1/devworkspaceoperatorconfig_types.go
+++ apis/controller/v1alpha1/devworkspaceoperatorconfig_types.go
@@ -203,6 +203,10 @@ type WorkspaceConfig struct {
 	// If the feature is disabled, setting this field may cause an endless workspace start loop.
 	// +kubebuilder:validation:Optional
 	HostUsers *bool `json:"hostUsers,omitempty"`
+	// InitContainers are injected into all workspace pods as Kubernetes init containers.
+	// Typical uses: injecting organization tools/configs, initializing persistent home, etc.
+	// Note: Only trusted administrators should be allowed to edit the DevWorkspaceOperatorConfig.
+	InitContainers []corev1.Container `json:"initContainers,omitempty"`
 }
 
 type WebhookConfig struct {

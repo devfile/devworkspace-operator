7c5c545a29de7b4657b4f10877f5c09409c36900
Oleksii Kurinnyi <okurinny@redhat.com>
2025-12-03 12:56:11 +0200

    fixup! fixup! Apply suggestion from @dkwon17
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>

--- pkg/library/home/custom_init_test.go
+++ pkg/library/home/custom_init_test.go
@@ -36,7 +36,7 @@ func TestCustomInitPersistentHome(t *testing.T) {
 		expectCustomInitSkipped bool
 	}{
 		{
-			name: "Skips default init when custom init-persistent-home is provided",
+			name: "Adds default init when custom init-persistent-home is provided",
 			workspace: &common.DevWorkspaceWithConfig{
 				DevWorkspace: &dw.DevWorkspace{
 					Spec: dw.DevWorkspaceSpec{
@@ -72,7 +72,7 @@ func TestCustomInitPersistentHome(t *testing.T) {
 					},
 				},
 			},
-			expectDefaultInitAdded: false,
+			expectDefaultInitAdded: true,
 		},
 		{
 			name: "Adds default init when no custom init-persistent-home is provided",

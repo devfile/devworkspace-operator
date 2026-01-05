fe3e2a70cc4b789bc79a61734f7f8137353a6f02
David Kwon <dakwon@redhat.com>
2026-01-05 15:35:28 -0500

    Add missing backslash in make-release.sh
    Signed-off-by: David Kwon <dakwon@redhat.com>

--- make-release.sh
+++ make-release.sh
@@ -142,7 +142,7 @@ update_images() {
   # Update defaults in generate_deployment.sh
   sed -i build/scripts/generate_deployment.sh -r \
     -e "s|quay.io/devfile/devworkspace-controller:[0-9a-zA-Z._-]+|${DWO_QUAY_IMG}|g" \
-    -e "s|quay.io/devfile/project-clone:[0-9a-zA-Z._-]+|${PROJECT_CLONE_QUAY_IMG}|g"
+    -e "s|quay.io/devfile/project-clone:[0-9a-zA-Z._-]+|${PROJECT_CLONE_QUAY_IMG}|g" \
     -e "s|quay.io/devfile/project-backup:[0-9a-zA-Z._-]+|${PROJECT_BACKUP_QUAY_IMG}|g"
 
   local DEFAULT_DWO_IMG="$DWO_QUAY_IMG"
